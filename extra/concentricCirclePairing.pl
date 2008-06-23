e5cc9371bee1f8bba69f7bd020b9c29b05db3eb9
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2008-06-23 14:00:10 +0000

    other unclassified scripts, etc, eg shuffling player from inner to outer ring and back to pair all players with each other

---
+++ extra/concentricCirclePairing.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl 
+
+use warnings;
+use strict;
+use integer;
+
+sub allocate_schedule
+{
+	my $N = shift;
+	my $n = ($N+1)/2;
+	my @inner = ();
+	my @outer = ();
+	$outer[$_] = $_ foreach 0 .. $n-1;
+	$inner[$_] = $n+$_ foreach 0 .. $n-1;
+	# $outer[$n-1] = 'bye' if $N % 2;
+	my $arrangement;
+	for my $twostep ( 0 .. $n ) 
+	{
+		my $shufflein;
+		for my $position ( 0 .. $n-1 ) 
+		{
+			$$shufflein[$inner[$position]] = $outer[$position];
+			$$shufflein[$outer[$position]] = $inner[$position];
+		}
+		push @$arrangement, $shufflein;
+		my $shuffler = $outer[$n-1];
+		$outer[$n-1] = $inner[$n-1];
+		$inner[$n-1] = $shuffler;
+		unshift @inner, pop @inner;
+
+		my $shuffleout;
+		for my $position ( 0 .. $n-1 ) 
+		{
+			$$shuffleout[$inner[$position]] = $outer[$position];
+			$$shuffleout[$outer[$position]] = $inner[$position];
+		}
+		push @$arrangement, $shuffleout;
+		$shuffler = $inner[0];
+		$inner[0] = $outer[0];
+		$outer[0] = $shuffler;
+		push @outer, shift @outer;
+
+	}
+	return $arrangement;
+}
+
+print allocate_schedule(8);
