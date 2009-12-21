d2de147fa7007dd4401838193f818fd7f14a675e
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:07:05 +0000

    r2458@e_learning:  drbean | 2009-11-20 21:05:34 +0800  Rolebearers belongs_to Role, allowing CP::Authorization::Roles to work. And with 'profile', also belongs_to Player.

--- competitions/quiz/trunk/Web/lib/Web/Schema/Result/Rolebearers.pm
+++ competitions/quiz/trunk/Web/lib/Web/Schema/Result/Rolebearers.pm
@@ -24,6 +24,9 @@ __PACKAGE__->set_primary_key("player", "role");
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:T3TmOtj4Cz53qIYPVYXMow
 
+__PACKAGE__->belongs_to(
+        role => 'Web::Schema::Result::Roles', 'role' );
+__PACKAGE__->belongs_to( profile =>'Web::Schema::Result::Players', 'player' );
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
