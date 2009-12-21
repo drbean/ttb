a654f7f7c254fe9ebc2853545ad6c63008fda1d1
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:06:52 +0000

    r2455@e_learning:  drbean | 2009-11-20 20:53:10 +0800  Members has_many roles

--- competitions/quiz/trunk/Web/lib/Web/Schema/Result/Members.pm
+++ competitions/quiz/trunk/Web/lib/Web/Schema/Result/Members.pm
@@ -29,6 +29,7 @@ __PACKAGE__->set_primary_key("league", "player");
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gJHWIeNlHGuuGlCZHyMJSQ
 
+# __PACKAGE__->has_many( roles => 'Web::Schema::Result::Roles', 'player' );
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
