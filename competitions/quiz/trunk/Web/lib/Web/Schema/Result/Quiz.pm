cf3f91260671cc33aa4442422ab8059253c3d272
drbean <drbean@users.noreply.github.com>
2009-12-21 09:07:52 +0000

    r2470@e_learning:  drbean | 2009-11-21 09:41:50 +0800  rm lib/Web/Schema/Result/Quiz.pm; mv lib/Web/Schema/Result/Exercises.pm lib/Web/Schema/Result/Quiz.pm

--- competitions/quiz/trunk/Web/lib/Web/Schema/Result/Quiz.pm
+++ competitions/quiz/trunk/Web/lib/Web/Schema/Result/Quiz.pm
@@ -1,4 +1,4 @@
-package Web::Schema::Result::Quiz;
+package Web::Schema::Result::Exercises;
 
 use strict;
 use warnings;
@@ -6,51 +6,49 @@ use warnings;
 use base 'DBIx::Class';
 
 __PACKAGE__->load_components("InflateColumn::DateTime", "Core");
-__PACKAGE__->table("quiz");
+__PACKAGE__->table("exercises");
 __PACKAGE__->add_columns(
-  "league",
+  "genre",
   {
     data_type => "VARCHAR",
     default_value => undef,
     is_nullable => 0,
     size => 15,
   },
-  "exercise",
+  "id",
   {
     data_type => "VARCHAR",
     default_value => undef,
     is_nullable => 0,
     size => 15,
   },
-  "player",
-  { data_type => "INT", default_value => undef, is_nullable => 0, size => undef },
-  "question",
+  "text",
   {
     data_type => "VARCHAR",
     default_value => undef,
     is_nullable => 0,
     size => 15,
   },
-  "answer",
+  "description",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 50,
+  },
+  "type",
   {
     data_type => "VARCHAR",
     default_value => undef,
     is_nullable => 0,
     size => 15,
-  },
-  "correct",
-  {
-    data_type => "TINYINT",
-    default_value => undef,
-    is_nullable => 0,
-    size => undef,
   },
 );
-__PACKAGE__->set_primary_key("exercise", "player", "question");
+__PACKAGE__->set_primary_key("genre", "id");
 
 
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:xnQ79X8dDLS13z2wcLWs/w
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Gy+JKFpLh9KriN3D4enzJw
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
