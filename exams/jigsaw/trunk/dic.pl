b4ded65803e6cf9b3056b6f87a12d6b5b3ee65f8
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 08:39:28 +0000

    r849@e_learning:  drbean | 2008-11-19 19:44:06 +0800  use qNumber instead of hardcoding it

--- exams/jigsaw/trunk/dic.pl
+++ exams/jigsaw/trunk/dic.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Last Edit: 2008 11月 18, 08時35分20秒
+# Last Edit: 2008 11月 19, 19時41分26秒
 # $Id: /dic/branches/ctest/dic.pl 1263 2007-06-23T12:37:20.810966Z greg  $
 
 use strict;
@@ -77,7 +77,7 @@ foreach my $group ( keys %$groups )
 "{\\myTFquestion
 {" . ( $_+1 ) . ". $questions->[$_]->[0]}
 {$questions->[$_]->[1]}
-{$questions->[$_]->[2]}}" } 0..6 ) . "\n";
+{$questions->[$_]->[2]}}" } 0..$qNumber-1 ) . "\n";
 		$tmplString .= "\\par\n";
 		$tmplString .= "\\end{textblock}\n";
 
