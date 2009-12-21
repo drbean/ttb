96e8fcd7bf9bdfea824a922769756806ca221f30
drbean <drbean@users.noreply.github.com>
2009-12-21 09:09:54 +0000

    r2503@e_learning:  drbean | 2009-11-23 11:32:25 +0800  Didn't remove genre from PK in r2479. has_many 'questions' accessor on Questions. Should be useful but deleting (not delete_all) Quiz deleted Questions.

--- competitions/quiz/trunk/Web/lib/Web/Schema/Result/Quiz.pm
+++ competitions/quiz/trunk/Web/lib/Web/Schema/Result/Quiz.pm
@@ -8,13 +8,6 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components("InflateColumn::DateTime", "Core");
 __PACKAGE__->table("quiz");
 __PACKAGE__->add_columns(
-  "genre",
-  {
-    data_type => "INT",
-    default_value => undef,
-    is_nullable => 0,
-    size => undef,
-  },
   "topic",
   {
     data_type => "VARCHAR",
@@ -36,13 +29,21 @@ __PACKAGE__->add_columns(
     is_nullable => 0,
     size => 50,
   },
+  "genre",
+  {
+    data_type => "INT",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
 );
-__PACKAGE__->set_primary_key("genre", "topic", "story");
+__PACKAGE__->set_primary_key("topic", "story");
 
 
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Gy+JKFpLh9KriN3D4enzJw
 
-
+__PACKAGE__->has_many( questions => 'Web::Schema::Result::Questions', {
+	'foreign.topic' => 'self.topic', 'foreign.story' => 'self.story' });
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
