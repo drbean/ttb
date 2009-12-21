0a85d22bba6ea2409416de70a759e9c39b9cbe8d
drbean <drbean@users.noreply.github.com>
2009-12-21 09:02:22 +0000

    r2229@e_learning:  drbean | 2009-09-28 14:49:12 +0800  ../ x 2, instead of .. ... In fact .. .. is better

--- competitions/quiz/trunk/Web/lib/Web/Model/DB.pm
+++ competitions/quiz/trunk/Web/lib/Web/Model/DB.pm
@@ -7,7 +7,7 @@ use Catalyst;
 use Web;
 
 my $name = Web->config->{database};
-my $db = Web->path_to( '..', '..', 'dic', 'db', $name );
+my $db = Web->path_to( '../' x 2, 'dic', 'db', $name );
 
 my $connect_info;
 if ( $^O eq 'linux' ) { $connect_info = [ "dbi:SQLite:$db", '', '', ]; }
