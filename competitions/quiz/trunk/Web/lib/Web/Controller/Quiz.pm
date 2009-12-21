4290c22c16c5a88d938a0387b7beae98587b6e6d
drbean <drbean@users.noreply.github.com>
2009-12-21 09:09:39 +0000

    r2498@e_learning:  drbean | 2009-11-22 21:27:22 +0800  Check, score actions from Play controller. genre is league->genre->genre->id, not name. TT seems to be accepting DBIC resultsets now from list action. (Can't find where I thought it wasn't). Redirect to list action after creating quiz. (Works in firefox if not in lynx.)

--- competitions/quiz/trunk/Web/lib/Web/Controller/Quiz.pm
+++ competitions/quiz/trunk/Web/lib/Web/Controller/Quiz.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 
+use List::Util qw/sum/;
+
 =head1 NAME
 
 dic::Controller::Exercises - Catalyst Controller
@@ -42,11 +44,10 @@ sub list : Local {
     my ($self, $c) = @_;
     my $leagueid = $c->session->{league};
     my $league = $c->model('DB::Leagues')->find({id=>$leagueid});
-    my $genre = $league->genre->genre->name;
+    my $genre = $league->genre->genre->id;
+    $c->stash->{quiz} = [$c->model('DB::Quiz')->search( { genre => $genre })];
     # Retrieve all of the text records as text model objects and store in
     # stash where they can be accessed by the TT template
-    $c->stash->{quiz} = [$c->model('DB::Quiz')->search(
-	    { genre => $genre })];
     # Set the TT template to use.  You will almost always want to do this
     # in your action methods (actions methods respond to user input in
     # your controllers).
@@ -85,10 +86,115 @@ sub create : Local {
 			story => $story,
 			description => $description,
 			});
-	$c->stash->{quiz_id} = $quizId;
-	$c->stash->{template} = 'quiz/list.tt2';
+	$c->response->redirect($c->uri_for('list',
+		   {status_msg => "Quiz added"}));
 }
-			
+
+
+=head2 check
+
+Check answers, show a page as record.
+
+=cut
+ 
+sub check : Local {
+	my ($self, $c, $topic, $story) = @_;
+	my $player = $c->session->{player_id};
+	my $target = $c->model('DB::Jigsawroles')->find({ player => $player });
+	my $targetId = $target? $target->role: 'all';
+	my $leagueId = $c->session->{league};
+	my $genre = $c->model("DB::Leaguegenre")->find(
+			{ league => $leagueId } )->genre;
+	my $questions = $genre->questions->search(
+		{ topic => $topic, story => $story } );
+	my (@record, @popquestions);
+	while ( my $question = $questions->next ) {
+		my $qid = $question->id;
+		my $response = $c->request->params->{$qid};
+		if ( my $played = $question->played->find({ player => $player,
+				league => $leagueId }) ) {
+			push @record, { question => $question->id,
+				content => $question->content,
+				response => $played->response,
+				newresponse => $response
+			};
+			$c->stash->{error_msg} =
+		"You already submitted your answers. They cannot be changed.";
+		}
+		else {
+			my $correct = $response eq $question->answer? 1: 0;
+			push @record, { question => $qid,
+				content => $question->content,
+				response => $response, };
+			push @popquestions, { league => $leagueId,
+				player => $player, topic => $topic,
+				story => $story, question => $qid,
+				response => $response };
+		}
+	}
+	$c->model('DB::Play')->populate( \@popquestions ) if @popquestions;
+	$c->stash->{questions} = \@record;
+	$c->stash->{genre} = $genre->name;
+	$c->stash->{target} = $targetId;
+	$c->stash->{template} = "record.tt2";
+}
+
+
+=head2 score
+
+Score answers, show a results page
+
+=cut
+ 
+sub score : Local {
+	my ($self, $c, $topic, $story) = @_;
+	my $player = $c->session->{player_id};
+	my $target = $c->model('DB::Jigsawroles')->find({ player => $player });
+	my $targetId = $target? $target->role: 'all';
+	my $leagueId = $c->session->{league};
+	my $genre = $c->model("DB::Leaguegenre")->find(
+			{ league => $leagueId } )->genre;
+	my $questions = $genre->questions->search(
+		{ topic => $topic, story => $story } );
+	my @quiz;
+	while ( my $q = $questions->next ) {
+		push @quiz, { content => $q->content, id => $q->id, };
+	}
+	my @quizids = map { $_->{id} } @quiz;
+	my ($scores, %totals, @playerids);
+	my $league = $c->model('DB::Leagues')->find({ id => $leagueId });
+	my $players = $league->members;
+	while ( my $player = $players->next ) {
+		my $pid = $player->profile->id;
+		push @playerids, $pid;
+		my $played = $player->play->search({
+				topic => $topic, story => $story });
+		while (my $question = $played->next ) {
+			my $profile = $question->profile;
+			my $qid = $profile->id;
+			my $correct = $profile->answer eq $question->response?
+					1: 0;
+			$scores->{$pid}->{$qid} = $correct;
+		}
+		$scores->{$pid}->{total} = sum map { $scores->{$pid}->{$_} }
+					keys %{ $scores->{$pid} };
+	}
+	my %questiontotals;
+	for my $qid ( @quizids ) {
+		for my $pid ( @playerids ) {
+			next unless defined $scores->{$pid}->{$qid};
+			$questiontotals{$qid} += $scores->{$pid}->{$qid};
+		}
+	}
+	$c->stash->{quiz} = \@quiz;
+	$c->stash->{scores} = $scores;
+	$c->stash->{totals} = \%questiontotals;
+	$c->stash->{genre} = $genre->name;
+	$c->stash->{target} = $targetId;
+	$c->stash->{template} = "scores.tt2";
+}
+
+
 =head2 delete
 
 Delete a quiz. Delete of Questions done here too.
@@ -99,8 +205,7 @@ sub delete : Local {
 	my ($self, $c, $topic, $story) = @_;
 	my $quiz = $c->model('DB::Quiz')->find({
 			topic => $topic, story => $story });
-	$quiz->delete_all;
-	$c->stash->{status_msg} = "Quiz deleted.";
+	$quiz->delete;
 	$c->response->redirect($c->uri_for('list',
 		   {status_msg => "Quiz deleted."}));
 }
