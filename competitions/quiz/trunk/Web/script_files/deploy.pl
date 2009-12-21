2b2189b778d01dcda3cdfe119af57796d2a9438e
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:02:50 +0000

    r2355@e_learning:  drbean | 2009-10-26 14:49:41 +0800  dic script_files to deploy, delete rows, dump tables, load YAML files and parts of YAML files with IDs and playerleagues (What's that doing here?)

---
+++ competitions/quiz/trunk/Web/script_files/deploy.pl
@@ -0,0 +1,52 @@
+#!perl
+
+use strict;
+use warnings;
+use lib 'lib';
+
+use Config::General;
+use Cwd;
+use File::Spec;
+
+BEGIN {
+	my @MyAppConf = glob( '*.conf' );
+	die "Which of @MyAppConf is the configuration file?"
+				unless @MyAppConf == 1;
+	my %config = Config::General->new($MyAppConf[0])->getall;
+	$::name = $config{name};
+	require "$::name.pm"; $::name->import;
+	require "$::name/Schema.pm"; $::name->import;
+}
+
+no strict qw/subs refs/;
+my $connect_info = "${::name}::Model::DB"->config->{connect_info};
+# my $connect_info = [ 'dbi:SQLite:db/demo','','' ];
+my $schema = "${::name}::Schema"->connect( @$connect_info );
+use strict;
+
+# $schema->deploy({ add_drop_table => 1});
+$schema->deploy;
+
+=head1 NAME
+
+deploy.pl - Set up db
+
+=head1 SYNOPSIS
+
+perl script_files/deploy.pl
+
+=head1 DESCRIPTION
+
+'CREATE TABLE players (id text, name text, password text, primary key (id))'
+
+=head1 AUTHOR
+
+Dr Bean, C<drbean at (@) cpan dot, yes a dot, org>
+
+=head1 COPYRIGHT
+
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ competitions/quiz/trunk/Web/script_files/playerleagues.pl
@@ -0,0 +1,156 @@
+#!perl
+
+use strict;
+use warnings;
+use lib 'lib';
+
+use Config::General;
+use Cwd;
+use File::Spec;
+use List::MoreUtils qw/all/;
+use YAML qw/LoadFile/;
+
+BEGIN {
+	my @MyAppConf = glob( '*.conf' );
+	die "Which of @MyAppConf is the configuration file?"
+				unless @MyAppConf == 1;
+	my %config = Config::General->new($MyAppConf[0])->getall;
+	$::name = $config{name};
+	require "$::name.pm"; $::name->import;
+	require "$::name/Schema.pm"; $::name->import;
+}
+
+my @leagueids = qw/GL00029 GL00030 GL00031 GL00034 FLA0016 MIA0017 BMA0099 BMA0100 FLA0030 FLA0027/;
+my $dir = ( File::Spec->splitdir(getcwd) )[-1];
+$dir = qr/^(GL000|FL|MIA|BMA)/ if $dir eq 'dic';
+@leagueids = grep m/$dir/, @leagueids;
+
+no strict qw/subs refs/;
+my $connect_info = "${::name}::Model::DB"->config->{connect_info};
+# my $connect_info = [ 'dbi:SQLite:db/demo','','' ];
+my $schema = "${::name}::Schema"->connect( @$connect_info );
+use strict;
+
+my $leagues = [
+		[ qw/id name field/ ],
+	[ "GL00029", "GL00029日語文共同學制虛擬班二", "中級英文聽說訓練" ],
+	[ "GL00030", "GL00030日語文共同學制虛擬班二", "中級英文聽說訓練" ],
+	[ "GL00031", "GL00031日語文共同學制虛擬班二", "中級英文聽說訓練" ],
+	[ "GL00034", "GL00034日語文共同學制虛擬班二", "中級英文聽說訓練" ],
+	[ "FLA0016", "FLA0016夜應外大學二甲", "英語會話" ],
+	[ "MIA0017", "MIA0012日資管大學二甲", "商用英文實務" ],
+	[ "BMA0099", "BMA0099日經管大學二甲", "商用英文實務" ],
+	[ "BMA0100", "BMA0100日經管大學二乙", "商用英文實務" ],
+	[ "FLA0030", "FLA0030夜應外大學四甲", "商用英文" ],
+	[ "FLA0027", "FLA0027夜應外大學二甲", "跨文化溝通" ],
+	[ "access", "Self-Access Learning", "Listening" ],
+	];
+
+uptodatepopulate( 'League', $leagues );
+
+my $leaguegenres = [
+			[ qw/league genre/ ],
+			[ "GL00029",	"intermediate" ],
+			[ "GL00030",	"intermediate" ],
+			[ "GL00031",	"intermediate" ],
+			[ "GL00034",	"intermediate" ],
+			[ "FLA0016",	"intermediate" ],
+			[ "MIA0017",	"business" ],
+			[ "BMA0099",	"business" ],
+			[ "BMA0100",	"business" ],
+			[ "FLA0030",	"business" ],
+			[ "FLA0027",	"intercultural" ],
+			[ "access",	"access" ],
+		];
+uptodatepopulate( 'Leaguegenre', $leaguegenres );
+
+my ($leaguefile, $players);
+
+for my $league ( 'GL00029', 'GL00030', 'GL00031', 'GL00034', 'FLA0016', 'MIA0017', 'BMA0099', 'BMA0100', 'FLA0030', 'FLA0027', ) {
+	$leaguefile = LoadFile "/home/drbean/class/$league/league.yaml";
+	push @{$players->{$league}},
+		map {[ $_->{id}, $_->{Chinese}, $_->{password} ]}
+					@{$leaguefile->{member}};
+}
+
+push @{$players->{officials}}, [split] for <<OFFICIALS =~ m/^.*$/gm;
+193001	DrBean	ok
+OFFICIALS
+
+my %players;
+foreach my $league ( 'officials', @leagueids )
+{
+	next unless $players->{$league} and ref $players->{$league} eq "ARRAY";
+	my @players = @{$players->{$league}};
+	foreach ( @players )
+	{
+		$players{$_->[0]} = [ $_->[0], $_->[1], $_->[2] ];
+	}
+}
+my $playerpopulator = [ [ qw/id name password/ ], values %players ];
+uptodatepopulate( 'Player', $playerpopulator );
+
+my (@allLeaguerolebearers, @allLeaguePlayers);
+foreach my $league ( @leagueids )
+{
+	my (%members, %rolebearers);
+	next unless $players->{$league} and ref $players->{$league} eq "ARRAY";
+	my @players = @{$players->{$league}};
+	foreach my $player ( @players )
+	{
+		$members{$player->[0]} =  [ $league, $player->[0] ];
+		$rolebearers{$player->[0]} =  [ $player->[0], 2 ];
+	}
+	push @allLeaguePlayers, values %members;
+	push @allLeaguerolebearers, values %rolebearers;
+	$members{193001} = [ $league, 193001 ];
+}
+uptodatepopulate( 'Member', [ [ qw/league player/ ], 
+				@allLeaguePlayers ] );
+
+uptodatepopulate( 'Role', [ [ qw/id role/ ], 
+[ 1, "official" ],
+[ 2, "player" ],
+[ 3, "amateur" ], ] );
+
+uptodatepopulate( 'Rolebearer', [ [ qw/player role/ ], 
+				[ 193001, 1 ],
+				@allLeaguerolebearers ] );
+
+sub uptodatepopulate
+{
+	my $class = $schema->resultset(shift);
+	my $entries = shift;
+	my $columns = shift @$entries;
+	foreach my $row ( @$entries )
+	{
+		my %hash;
+		@hash{@$columns} = @$row;
+		$class->update_or_create(\%hash);
+	}
+}
+
+
+=head1 NAME
+
+script_files/playerleagues.pl.pl - populate leagues, players, members, roles, rolebrarer tables
+
+=head1 SYNOPSIS
+
+perl script_files/playerleagues.pl
+
+=head1 DESCRIPTION
+
+INSERT INTO players (id, name, password) VALUES (?, ?, ?)
+
+=head1 AUTHOR
+
+Dr Bean, C<drbean at (@) cpan dot, yes a dot, org>
+
+=head1 COPYRIGHT
+
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
