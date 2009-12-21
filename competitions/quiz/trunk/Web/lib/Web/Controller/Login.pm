54a181638f3f088183ae3a9a1b56e7c99b33b45d
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:02:16 +0000

    r2216@e_learning:  drbean | 2009-09-27 11:14:15 +0800  deleted handcopied Login and cp  //loc/dic/trunk/lib/dic/Controller/Login.pm lib/Web/Controller/Login.pm

--- competitions/quiz/trunk/Web/lib/Web/Controller/Login.pm
+++ competitions/quiz/trunk/Web/lib/Web/Controller/Login.pm
@@ -6,21 +6,24 @@ use parent 'Catalyst::Controller';
 
 =head1 NAME
 
-Web::Controller::Login - Login Controller for Web
+Web::Controller::Login - Catalyst Controller
 
 =head1 DESCRIPTION
 
-[enter your description here]
+Catalyst Controller.
 
 =head1 METHODS
 
 =cut
 
-=head2 login
+
+=head2 index
+
+Login logic. We used to let "guest"s in without a password, or ID and also redirect to exercise list. Now we redirect to the exercise, if it appears as the one argument.
 
 =cut
 
-sub login :Path :Args(0)  {
+sub index :Path :Args(0)  {
     my ( $self, $c ) = @_;
     my $id       = $c->request->params->{id}       || "";
     my $name     = $c->request->params->{name}     || "";
@@ -30,8 +33,17 @@ sub login :Path :Args(0)  {
         if ( $c->authenticate( { id => $username, password => $password } ) ) {
             $c->session->{player_id} = $id;
             $c->session->{question} = undef;
+            my $officialrole = 1;
+            if ( $c->check_user_roles($officialrole) ) {
+                $c->stash->{id}   = $id;
+                $c->stash->{name} = $name;
+                $c->stash->{leagues} =
+                  [ $c->model('DB::League')->search( {} ) ];
+                $c->stash->{template} = 'official.tt2';
+                return;
+            }
             my @memberships =
-              $c->model("DB::Members")->search( { player => $id } );
+              $c->model("DB::Member")->search( { player => $id } );
             my @leagues;
             for my $membership (@memberships) {
                 push @leagues, $membership->league;
@@ -46,15 +58,15 @@ sub login :Path :Args(0)  {
             else {
                 $c->session->{league}   = $leagues[0]->id;
 		if ( defined $c->session->{exercise}) {
-		    my $exercise = $c->session->{exercise};
-		    $c->response->redirect(
-			$c->uri_for( "/play/update/$exercise" ) );
+			my $exercise = $c->session->{exercise};
+			$c->response->redirect(
+				$c->uri_for( "/play/update/$exercise" ) );
 		}
 		else {
 			$c->response->redirect( $c->uri_for("/exercises/list") );
 		}
-		return;
-	    }
+                return;
+            }
         }
         else {
             $c->stash->{error_msg} = "Bad username or password.";
@@ -66,24 +78,61 @@ sub login :Path :Args(0)  {
     $c->stash->{template} = 'login.tt2';
 }
 
-=head2 end
+=head2 official
 
-Attempt to render a view, if needed.
+Set league official is organizing. Use session player_id to authenticate the participant.
 
 =cut
 
-sub end : ActionClass('RenderView') {}
+sub official : Local {
+	my ($self, $c) = @_;
+	my $league = $c->request->params->{league} || "";
+	my $password = lc $c->request->params->{password} || "";
+        my $username = $c->session->{player_id};
+        if ( $c->authenticate( {id =>$username, password=>$password} ) ) {
+		# my $officialrole = "official";
+		my $officialrole = 1;
+		if ( $c->check_user_roles($officialrole) ) {
+			$c->session->{league} = $league;
+			$c->response->redirect($c->uri_for("/exercises/list"));
+			return;
+		}
+		else {
+		# Set an error message
+		$c->stash->{error_msg} = "Bad username or password?";
+		$c->stash->{template} = 'login.tt2';
+		}
+	}
+	$c->stash->{template} = 'login.tt2';
+}
+
+
+=head2 membership
+
+Set league multi-membership player is participating in.
+
+=cut
+
+sub membership : Local {
+	my ($self, $c) = @_;
+	my $league = $c->request->params->{league} || "";
+	my $password = $c->request->params->{password} || "";
+	$c->session->{league} = $league;
+	$c->session->{exercise} = undef;
+	$c->response->redirect( $c->uri_for("/exercises/list") );
+	return;
+}
+
 
 =head1 AUTHOR
 
-Dr Bean
+Dr Bean,,,
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
+This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
 
 1;
-# vim: set ts=8 sts=4 sw=4 noet:
