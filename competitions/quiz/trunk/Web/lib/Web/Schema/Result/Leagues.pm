212d17dfe67a7ac643a0126264c16469ecaa5a73
drbean <drbean@users.noreply.github.com>
2010-02-15 11:35:04 +0000

    r1988@momotaro:  greg | 2010-02-15 12:07:46 +0900  Leagues, Leaguegenre, Genre relationship looks like Players, Members, Leagues, but League has_one Leaguegenre. Had problems getting many_to_many to work. Changed Leaguegenre's 'genre' belongs_to relationship on Genre to 'data'. Changed Leagues' 'genre' belongs_to relationship on League to has_one relationship. Added 'leaguegenre' has_many relationship on Leaguegenre to Genre.

--- competitions/quiz/trunk/Web/lib/Web/Schema/Result/Leagues.pm
+++ competitions/quiz/trunk/Web/lib/Web/Schema/Result/Leagues.pm
@@ -36,7 +36,7 @@ __PACKAGE__->set_primary_key("id");
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:LOzcmKBw2qRaekGGclQtKQ
 
-__PACKAGE__->belongs_to( genre => 'Web::Schema::Result::Leaguegenre', 
+__PACKAGE__->has_one( genre => 'Web::Schema::Result::Leaguegenre', 
 	 { 'foreign.league' => 'self.id' } );
 __PACKAGE__->has_many( members => 'Web::Schema::Result::Members', 'league' );
 # You can replace this text with custom content, and it will be preserved on regeneration
