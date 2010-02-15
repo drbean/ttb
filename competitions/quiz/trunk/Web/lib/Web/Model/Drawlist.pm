3371e00fe6d48d255532b00a68b7d7d179e06fb0
drbean <drbean@users.noreply.github.com>
2010-02-15 11:34:52 +0000

    r1986@momotaro:  greg | 2010-02-15 11:59:02 +0900  No need for 'lib'. Must config algorithm in BEGIN block and require Card, as well as Contestant::Swiss and Swiss. Must define swiss player before creating games. So opponents first, then unpaired, last bye. Then construct Games::Tournament::Card games.

--- competitions/quiz/trunk/Web/lib/Web/Model/Drawlist.pm
+++ competitions/quiz/trunk/Web/lib/Web/Model/Drawlist.pm
@@ -1,13 +1,10 @@
 package Web::Model::Drawlist;
 
-# Last Edit: 2010  2月 13, 17時46分40秒
+# Last Edit: 2010  2月 14, 20時54分57秒
 # $Id$
 
 use strict;
 use warnings;
-
-use FindBin qw/$Bin/;
-use lib "$Bin/../../../swiss/web/lib";
 
 =head1 NAME
 
@@ -22,9 +19,11 @@ Moose role Catalyst::Component::InstancePerContext allows passing of Catalyst co
 use Moose;
 with 'Catalyst::Component::InstancePerContext';
 
-require Games::Tournament::Swiss;
+BEGIN { $Games::Tournament::Swiss::Config::algorithm =
+							'Games::Tournament::Swiss::Procedure::FIDE'; }
 require Games::Tournament::Contestant::Swiss;
-
+require Games::Tournament::Swiss;
+require Games::Tournament::Card;
 
 =head2 drawlist
 
@@ -39,32 +38,50 @@ sub drawlist {
 	my $round = $league->round->round;
 	my $opponents = $league->opponents->search({ round => $round });
 	my $roles = $league->roles->search({ round => $round });
-	my ( @games, @unpaired, %seen );
+	my $scores = $league->scores;
+	my $ratings = $league->ratings->search({ round => $round -1 });
+	my ( @games, @lineup, @unpaired, %seen );
 	while ( my $pair = $opponents->next ) {
-		if ( $pair->opponent =~ m/bye/i ) {
-			$c->stash->{byegame} = { contestants => {Bye => $pair->ego->name} };
+		my $player = $pair->ego;
+		my $id = $player->id;
+		my $rating = $ratings->find({ player => $id })->value;
+		my $score = $scores->find({ player => $id })->score;
+		my $swissplayer = Games::Tournament::Contestant::Swiss->new( id => $id,
+			name => $player->name, rating => $rating, score => $score );
+		if ( $pair->opponent !~ m/unpaired|bye/i ) {
+			my $opponent = $pair->other;
+			my $oppid = $opponent->id;
+			if ( $seen{ $id } ) {
+				die "${id}'s opponent is $oppid, but is ${oppid}'s opponent $id?
+					" unless $seen{$oppid} == 1 and $seen{$id} == 1;
+				next;
+			}
+			my $swissopponent = Games::Tournament::Contestant::Swiss->new(
+				id => $oppid, name => $opponent->name,
+					rating => $ratings->find({ player => $oppid })->value,
+					score => $scores->find({ player => $oppid })->score );
+			push @lineup, $swissplayer, $swissopponent;
+			$seen{$id}++;
+			$seen{$oppid}++;
+			my $roleplayer = $roles->find({ player => $id })->role;
+			my $roleopponent = $roles->find({ player => $oppid })->role;
+			push @games, { contestants => { $roleplayer => $swissplayer,
+					$roleopponent => $swissopponent } };
+		}
+		elsif ( $pair->opponent =~ m/bye/i ) {
+			push @lineup, $swissplayer;
+			push @games, { contestants => {Bye => $swissplayer} };
 		}
 		elsif ( $pair->opponent =~ m/unpaired/i ) {
 			push @unpaired, $pair->player;
 		}
 		else {
-			my $player = $pair->ego;
-			my $opponent = $pair->other;
-			if ( $seen{ $player->id } ) {
-				die $player->id . "'s opponent is " . $opponent->id .
-				 ", but is " . $opponent->id . "'s opponent " . $player->id .
-				 "?" unless $seen{$opponent->id} == 1 and
-							 $seen{$player->id} == 1;
-				next;
-			}
-			$seen{$player->id}++;
-			$seen{$opponent->id}++;
-			my $roleplayer = $roles->find({ player => $player->id })->role;
-			my $roleopponent = $roles->find({ player => $opponent->id })->role;
-			push @games, { contestants => { $roleplayer => $player,
-					$roleopponent => $opponent } };
+			die "$id has no opponent, but no bye and not unpaired.";
 		}
 	}
+	@games = map { Games::Tournament::Card->new( %$_ ) } @games;
+	my $tourney = Games::Tournament::Swiss->new( entrants => \@lineup );
+	@games = $tourney->orderPairings( @games );
 	return [ $round, \@games, \@unpaired ];
 }
 
