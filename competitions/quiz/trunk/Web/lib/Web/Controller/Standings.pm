fa956762348e4e4c063c6414ea70c63218be11a8
drbean <drbean@users.noreply.github.com>
2010-08-31 07:03:52 +0000

    r3566@momotaro:  greg | 2010-08-31 15:50:47 +0900  Construct { opponent, role, point } triple for each player in each round, on basis of win, forfeit, tardy columns in Matches. (Ported to updatescores.pl.) 'position' hash maps ids to place. Change round var to rounds (probably wasn't necessary.)

--- competitions/quiz/trunk/Web/lib/Web/Controller/Standings.pm
+++ competitions/quiz/trunk/Web/lib/Web/Controller/Standings.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 
-use List::Util qw/sum/;
+use List::Util qw/sum first/;
 
 =head1 NAME
 
@@ -23,7 +23,7 @@ Catalyst Controller to show results of a number of rounds. This could be a Swiss
 
 http://server.school.edu/comp/standings
 
-Display results up to the present round.
+Display results up to the present round. TODO Tardy, forfeit points not being shown.
 
 =cut
 
@@ -32,38 +32,95 @@ sub index : Local {
 	my $tourid = $c->session->{league};
 	my $tournament = $c->model('SwissDB::Tournaments')->find(
 		{ id => $tourid });
-	my $round = $tournament->round->value;
+	my $rounds = $tournament->round->value;
 	my $members = $tournament->members;
 	my @columns = Swiss::Schema::Result::Players->columns;
 	my (%playerlist, @absentees);
 	while ( my $member = $members->next ) {
 		my $player = { map { $_ => $member->profile->$_ } @columns };
+		$player->{score} = $member->score->value;
 		$player->{firstround} = $member->firstround;
 		my $rating = $member->profile->rating->find({
-				tournament => $tourid, round => $round-1 });
+				tournament => $tourid, round => $rounds-1 });
 		$player->{rating} = $rating->value;
 		$playerlist{ $player->{id} } = $player;
 		push @absentees, $player if $member->absent eq 'True';
 	}
+	my @players = sort { $b->{score} <=> $a->{score} } values %playerlist;
+	my %position;
+	@position{ map { $_->{id} } @players } = ( 1 .. @players );
 	my @Roles = qw/White Black/;
 	my @roles = map { lcfirst $_ } @Roles;
 	my $matches = $tournament->matches;
-	my $games;
-	for my $round ( 1 .. $round ) {
+	my $play;
+	for my $round ( 1 .. $rounds ) {
 		my @games;
-		my $matches = $matches->search({ round => $round });
-		while ( my $match = $matches->next ) {
-			my %contestants = map { ucfirst($_) =>
-				$playerlist{ $match->$_ } } @roles;
-			push @games, {contestants => \%contestants,
-					win => $match->win,
-					forfeit => $match->forfeit };
+		my @matches = $matches->search({ round => $round })->all;
+		for my $match ( @matches ) {
+			my %contestant = map { ucfirst($_) =>
+							$match->$_ } @roles;
+			my %opponent; @opponent{ 'White', 'Black' } =
+				@contestant{ 'Black', 'White' };
+			if ( $contestant{Black} eq 'Bye' ) {
+				$play->{$round}->{ $contestant{White} }
+					->{opponent} = 'Bye';
+				$play->{$round}->{ $contestant{White} }->{role}
+					= 'Bye';
+				$play->{$round}->{ $contestant{White} }->{point}
+					= 'Bye';
+				next;
+			}
+			my $forfeit = $match->forfeit;
+			unless ( $forfeit eq 'None' or $forfeit eq 'Unknown' ) {
+				my @forfeiters = $forfeit eq 'Both'? @Roles:
+					( $forfeit );
+				for ( @forfeiters ) {
+					$play->{$round}->{ $contestant{$_} }
+						->{opponent} = $position{$opponent{$_}};
+					$play->{$round}->{ $contestant{$_} }
+						->{role} = $_;
+					$play->{$round}->{ $contestant{$_} }
+						->{point} = 'Forfeit';
+				}
+			}
+			my $tardy = $match->tardy;
+			unless ( $tardy eq 'None' or $tardy eq 'Unknown' ) {
+				my @tardies = $tardy eq 'Both'? @Roles:
+					( $tardy );
+				for ( @tardies ) {
+					$play->{$round}->{ $contestant{$_} }
+						->{opponent} = $position{$opponent{$_}};
+					$play->{$round}->{ $contestant{$_} }
+						->{role} = $_;
+					$play->{$round}->{ $contestant{$_} }
+						->{point} = 'Tardy';
+				}
+			}
+			my $win = $match->win;
+			unless ( $win eq 'None' or $win eq 'Unknown' ) {
+				my %points = $win eq 'White'?
+					( White => 'Win', Black => 'Loss' ):
+					$win eq 'Black'?
+					( White => 'Loss', Black => 'Win' ):
+					$win eq 'Both'?
+					( White => 'Draw', Black => 'Draw' ):
+					( White => '??', Black => '??' );
+				for ( @Roles ) {
+					$play->{$round}->{ $contestant{$_} }
+					->{opponent} = $position{$opponent{$_}};
+				$play->{$round}->{ $contestant{$_} }
+					->{role} = $_;
+				$play->{$round}->{ $contestant{$_} }
+					->{point} = $points{$_};
+				}
+			}
 		}
-		$games->{$round} = \@games;
 	}
 	$c->stash->{tournament} = $tourid;
 	$c->stash->{roles} = \@Roles;
-	$c->stash->{games} = $games;
+	$c->stash->{play} = $play;
+	$c->stash->{players} = \@players;
+	$c->stash->{rounds} = $rounds;
 	$c->stash->{template} = "standings.tt2";
 }
 
