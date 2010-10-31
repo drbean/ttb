25730c89dffc8caf41a9f0fbe2b5228a61bfe4e8
drbean <drbean@users.noreply.github.com>
2010-10-31 11:54:31 +0000

    conversion of /scratch/rc/score_response.rc to script to tally Q/A self-evaluation points. But what is script with same name doing in script_files. Looks like score_jigsaw, not score_comp

---
+++ competitions/quiz/trunk/paper/script_files/score_comp.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl 
+
+# Created: 西元2010年10月31日 19時06分22秒
+# Last Edit: 2010 10月 31, 19時54分01秒
+# $Id$
+
+=head1 NAME
+
+score_comp.pl - Question/answer self-evaluations tallies at each table
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+use IO::All;
+use YAML qw/LoadFile Dump/;
+use List::Util qw/sum/;
+use Cwd; use File::Basename;
+use Grades;
+
+
+=head1 SYNOPSIS
+
+score_comp.pl -l . -r 10 > comp/10/scores.yaml
+
+=cut
+
+
+my $leagues = "/home/drbean/class";
+
+my $script = Grades::Script->new_with_options;
+my $id = $script->league || basename( getcwd );
+my $round = $script->round;
+
+my $league = League->new( leagues => $leagues, id => $id );
+my $g = Compcomp->new( league => $league );
+
+my $dir = $g->compcompdirs;
+my $responses = LoadFile "$dir/$round/response.yaml";
+
+my %scores = map	{
+			    my $table = $_; my $r = $responses->{$table};
+			    $table =>
+			    { map	{
+					    my $p = $r->{$_}; $_ =>
+					    sum (	values( %{$p->{q}} ),
+							values( %{$p->{a}} ) )
+					} keys %$r }
+			} keys %$responses;
+
+print Dump \%scores;
+
+=head1 DESCRIPTION
+
+Contestants at each table rate their questions and answers. comptron.pl produces a score card, response.yaml. score_comp.pl tallies the points contestants have given themselves at each of the tables in the round.
+
+=cut
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# End of score_comp.pl
+
+# vim: set ts=8 sts=4 sw=4 noet:
+
+
