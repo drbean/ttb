2af21f1ee0ea472ab9e17630b14962c5934a791c
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2012-01-04 09:30:04 +0000

    tennis points: Fault, Ace, Rally

--- competitions/quiz/trunk/web/script_files/comptron.pl
+++ competitions/quiz/trunk/web/script_files/comptron.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 
 # Created: 西元2010年02月23日 22時33分13秒
-# Last Edit: 2011 Nov 06, 08:43:44 PM
+# Last Edit: 2012 Jan 04, 05:30:25 PM
 # $Id$
 
 =head1 NAME
@@ -92,11 +92,9 @@ for my $pair ( @pairs ) {
     next if $black eq 'Bye';
     my %questions; @questions{1..$qn } = ( undef ) x $qn;
     my $free = {
-	    $white => { q => { 1 => undef} , a => { 1 => undef } },
-	    $black => { q => { 1 => undef} , a => { 1 => undef } }
+	    $white => { point => { 1 => undef} },
+	    $black => { point => { 1 => undef} }
 	     };
-    Bless( $free->{$white} )->keys( [ 'q', 'a' ] );
-    Bless( $free->{$black} )->keys( [ 'q', 'a' ] );
     Bless( $free )->keys( [ $white, $black ] );
     my $set = { $white => \%questions,
 		$black => \%questions };
