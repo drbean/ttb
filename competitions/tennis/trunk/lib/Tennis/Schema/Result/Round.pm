32130b59b51d3986d4706d3b480a1999c9820065
drbean <drbean@users.noreply.github.com>
2012-02-03 02:05:55 +0000

    remirrored http://svn.openfoundry.org/ttb/competitions/tennis to //tennis, will it work

---
+++ competitions/tennis/trunk/lib/Tennis/Schema/Result/Round.pm
@@ -0,0 +1,135 @@
+package Tennis::Schema::Result::Round;
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use namespace::autoclean;
+extends 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp");
+
+=head1 NAME
+
+Tennis::Schema::Result::Match
+
+=cut
+
+__PACKAGE__->table("round");
+
+=head1 ACCESSORS
+
+=head2 white
+
+  data_type: 'VARCHAR(13)'
+  is_nullable: 1
+
+=head2 black
+
+  data_type: 'VARCHAR(13)'
+  is_nullable: 1
+
+=head2 league
+
+  data_type: 'VARCHAR(13)'
+  is_nullable: 0
+
+=head2 exercise
+
+  data_type: ''VARCHAR(13)
+  is_nullable: 0
+
+=head2 table
+
+  data_type: 'TINYINT'
+  is_nullable: 1
+
+=head2 games
+
+  data_type: 'TINYINT'
+  is_nullable: 1
+
+=head2 winner
+
+  data_type: 'BOOL'
+  is_nullable: 1
+
+=head2 forfeit
+
+  data_type: 'VARCHAR(13)'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "white",
+  { data_type => "VARCHAR(13)", is_nullable => 0 },
+  "black",
+  { data_type => "VARCHAR(13)", is_nullable => 0 },
+  "league",
+  { data_type => "VARCHAR(13)", is_nullable => 0 },
+  "exercise",
+  { data_type => "VARCHAR(13)", is_nullable => 0 },
+  "table",
+  { data_type => "TINYINT", is_nullable => 0 },
+  "games",
+  { data_type => "TINYINT", is_nullable => 0 },
+  "receiverpoints",
+  { data_type => "TINYINT", is_nullable => 0 },
+  "winner",
+  { data_type => "BOOL", is_nullable => 0 },
+  "forfeit",
+  { data_type => "VARCHAR(13)", is_nullable => 0 },
+);
+__PACKAGE__->set_primary_key("table", "white", "black", "exercise", "league");
+
+#=head1 RELATIONS
+#
+#=head2 played_by
+#
+#Type: belongs_to
+#
+#Related object: L<Bett::Schema::Result::Member>
+#
+#=cut
+#
+#__PACKAGE__->belongs_to(
+#  "played_by",
+#  "Bett::Schema::Result::Member",
+#  {	"foreign.player" => "self.player",
+#  	"foreign.league" => "self.league"
+#  },
+#  {
+#    is_deferrable => 0,
+#    join_type     => "LEFT",
+#    on_delete     => "CASCADE",
+#    on_update     => "CASCADE",
+#  },
+#);
+#
+#=head2 yn
+#
+#Type: belongs_to
+#
+#Related object: L<Bett::Schema::Result::Yn>
+#
+#=cut
+#
+#__PACKAGE__->belongs_to(
+#  "yns",
+#  "Bett::Schema::Result::Yn",
+#  {   "foreign.player" => "self.player",
+#      "foreign.league" => "self.league",
+#      "foreign.exercise" => "self.exercise"
+#  },
+#  {
+#    is_deferrable => 0,
+#    join_type     => "LEFT",
+#    on_delete     => "CASCADE",
+#    on_update     => "CASCADE",
+#  },
+#);
+
+__PACKAGE__->meta->make_immutable;
+1;
