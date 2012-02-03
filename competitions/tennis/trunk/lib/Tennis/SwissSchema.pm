43fed82ef9e795257e59bbdaad0aeb5c08d4b810
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2012-02-03 03:58:03 +0000

    failed with top-level cp io

---
+++ competitions/tennis/trunk/lib/Tennis/SwissSchema.pm
@@ -0,0 +1,21 @@
+package CompComp::SwissSchema;
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+
+use lib "$Bin/../../../swiss/web/lib";
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces(
+	result_namespace => '+Swiss::Schema::Result'
+	);
+
+
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-09-22 15:03:53
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:iWpEslRAR3iF6A9uKxwYoQ
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
