ab4a25a9c2ec2e62314a07de27e31e524da0a733
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2013-01-28 03:24:46 +0000

    Moose

--- competitions/quiz/trunk/web/lib/CompComp/Controller/Root.pm
+++ competitions/quiz/trunk/web/lib/CompComp/Controller/Root.pm
@@ -1,8 +1,8 @@
 package CompComp::Controller::Root;
 
-use strict;
-use warnings;
-use parent 'Catalyst::Controller';
+use Moose;
+use namespace::autoclean;
+BEGIN { extends 'Catalyst::Controller' }
 
 #
 # Sets the actions in this controller to be registered with no prefix
@@ -40,18 +40,18 @@ sub default :Path {
 }
 
 sub auto : Private {
-	my ($self, $c) = @_;
-	my $quiz = $c->request->query_params->{quiz};
-	$c->session->{quiz} = $quiz if $quiz;
-	if ($c->controller eq $c->controller('Login')) {
-	   return 1;
-	}
-	if (!$c->user_exists) {
-	   $c->log->debug('***Root::auto User not found, forwarding to /login');
-	   $c->response->redirect($c->uri_for('/login'));
-	   return 0;
-       }
-       return 1;
+    my ($self, $c) = @_;
+    my $quiz = $c->request->query_params->{quiz};
+    $c->session->{quiz} = $quiz if $quiz;
+    if ($c->controller eq $c->controller('Login')) {
+	return 1;
+    }
+    if (!$c->user_exists) {
+        $c->log->debug('***Root::auto User not found, forwarding to /login');
+        $c->response->redirect($c->uri_for('/login'));
+        return 0;
+    }
+    return 1;
 }
 
 =head2 end
@@ -72,6 +72,8 @@ This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+_PACKAGE->meta->make_immutable;
 
 1;
 # vim: set ts=8 sts=4 sw=4 noet:
