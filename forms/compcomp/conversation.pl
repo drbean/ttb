1db8b0c20a3f4c319cdcdd3bfe903d9a24d86950
drbean <drbean@users.noreply.github.com>
2013-02-10 05:40:15 +0000

    Generate groups, champions, letter. names, ids

---
+++ forms/compcomp/conversation.pl
@@ -0,0 +1,99 @@
+\documentclass[a4paper]{article}
+\newcommand{\printlandscape}{\special{landscape}}
+\printlandscape
+\usepackage[landscape]{geometry}
+\usepackage{multicol}
+\usepackage[usenames,dvipsnames]{color}
+\usepackage[absolute]{textpos}
+% \usepackage[absolute,showboxes]{textpos}
+% \textblockorigin{0.00cm}{+0.15cm} %HP DeskJet 5160
+\textblockorigin{0.00cm}{0.00cm} %HPDeskJet5160
+% \textblockorigin{0.00cm}{0.50cm} %HPDeskJet5160
+% \textblockorigin{0.15cm}{0.00cm} %HPDeskJet5160
+\pagestyle{empty}
+\setlength{\unitlength}{1cm}
+
+\newcommand{\mycard}[2]{%
+\TPMargin{0.0cm}
+	\begin{textblock}{4}(#1)
+% \textblockrulecolor{red}
+	\textblocklabel{picture1}
+	\vspace{0.25cm}
+\begin{minipage}{7.0cm}%
+	\raggedleft \large Conversation competition \hfill \normalsize \raisebox{-0.2cm}{Week: \rule{1.0cm}{0.3pt}} \\
+	\vspace{-0.05cm}
+	\large I. \rule{2.0cm}{0.3pt} \& II. \rule{2.0cm}{0.3pt} Groups\\
+	\raggedright
+	\normalsize Story: \rule{1.4cm}{0.3pt} \hfill \\
+	\vspace{-0.05cm}
+	\large Champions (Letter \rule{0.5cm}{0.3pt}):\\
+	\vspace{0.09cm}
+	\normalsize I. \rule{1.1cm}{0.3pt} Champion: \rule{1.3cm}{0.3pt}\\
+	\vspace{0.25cm}
+	\normalsize II. \rule{1.0cm}{0.3pt} Champion: \rule{1.3cm}{0.3pt}\\
+	\vspace{0.25cm}
+	\large Winner: \rule{2.1cm}{0.3pt}\\
+	\large Votes:\\
+	\normalsize I. \rule{1.2cm}{0.3pt} Names: \rule{1.1cm}{0.3pt}, \rule{1.1cm}{0.3pt}, \rule{1.1cm}{0.3pt}\\
+	\vspace{0.25cm}
+	\normalsize II. \rule{1.1cm}{0.3pt} Names: \rule{1.1cm}{0.3pt}, \rule{1.1cm}{0.3pt}, \rule{1.1cm}{0.3pt}\\
+	\large Reasons:\\
+	\normalsize Winner's Supporters\\
+	\tiny Name: \rule{1.0cm}{0.3pt} \normalsize Reason: \rule{3.7cm}{0.3pt}\\
+	\vspace{0.25cm}
+	\tiny Name: \rule{1.0cm}{0.3pt} \normalsize Reason: \rule{3.7cm}{0.3pt}\\
+	\vspace{0.25cm}
+	\tiny Name: \rule{1.0cm}{0.3pt} \normalsize Reason: \rule{3.7cm}{0.3pt}\\
+	\normalsize Loser's Supporters\\
+	\tiny Name: \rule{1.0cm}{0.3pt} \normalsize Reason: \rule{3.7cm}{0.3pt}\\
+	\Large Points:\\
+	\large 3 points: \rule{1.6cm}{0.3pt} 2 points: \rule{1.8cm}{0.3pt}\\
+\end{minipage}
+	\end{textblock}
+\TPMargin{0.0cm}
+	
+	\TPshowboxestrue
+	\begin{textblock}{1.2}(#2)
+	\normalsize In \& Out
+	\vspace{-0.2cm}
+	\begin{description}
+	\item [Absent] \hfill \\
+	\item [Joining] \hfill \\
+	\end{description}
+	\vspace{0.2cm}
+	\end{textblock}
+	\TPshowboxesfalse
+	}
+
+\begin{document}
+% \fontfamily{hlst}\fontseries{b}\fontshape{n}\selectfont
+
+\mycard{0,0}{2.8,1.0} \mycard{4,0}{6.8,1.0} \mycard{8,0}{10.8,1.0} \mycard{12,0}{14.8,1.0}
+\mycard{0,8}{2.8,9.0} \mycard{4,8}{6.8,9.0} \mycard{8,8}{10.8,9.0} \mycard{12,8}{14.8,9.0}
+
+			% crosshairs cutting guide marks
+			\begin{textblock}{1}(3.64,8)\rule{3mm}{0.1mm}%
+			\end{textblock}
+			\begin{textblock}{1}(7.64,8)\rule{3mm}{0.1mm}%
+			\end{textblock}
+			\begin{textblock}{1}(11.64,8)\rule{3mm}{0.1mm}%
+			\end{textblock}
+
+			\begin{textblock}{1}(3.70,4)\rule{0.1mm}{3mm}%
+			\end{textblock}
+			\begin{textblock}{1}(3.70,12)\rule{0.1mm}{3mm}%
+			\end{textblock}
+
+			\begin{textblock}{1}(7.70,4)\rule{0.1mm}{3mm}%
+			\end{textblock}
+			\begin{textblock}{1}(7.70,12)\rule{0.1mm}{3mm}%
+			\end{textblock}
+
+			\begin{textblock}{1}(11.70,4,92)\rule{0.1mm}{3mm}%
+			\end{textblock}
+			\begin{textblock}{1}(11.70,12)\rule{0.1mm}{3mm}%
+			\end{textblock}
+
+\null\newpage
+
+\end{document}
