89feb67afafd7876238e4c1a0e201d10d6388159
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2013-02-12 12:59:57 +0000

    textpos, week, story, letter, groups, champions args to mycard newcommand. paging.

--- forms/compcomp/conversation.pl
+++ forms/compcomp/conversation.pl
@@ -1,11 +1,65 @@
-	\raggedright
-	\normalsize Story: \rule{1.4cm}{0.3pt} \hfill \\
-	\vspace{-0.05cm}
-	\large Champions (Letter \rule{0.5cm}{0.3pt}):\\
-	\vspace{0.09cm}
-	\normalsize I. \rule{1.1cm}{0.3pt} Champion: \rule{1.3cm}{0.3pt}\\
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use Cwd; use File::Basename;
+use YAML qw/LoadFile DumpFile/;
+use IO::All;
+
+use Getopt::Long;
+use Pod::Usage;
+
+use Grades;
+
+my $script = Grades::Script->new_with_options;
+my $tourid = $script->league || basename( getcwd );
+( my $leagueid = $tourid ) =~ s/^([[:alpha:]]+[[:digit:]]+).*$/$1/;
+my $league = League->new( id => $leagueid );
+my $approach = $league->approach;
+my $g = Grades->new( league => $league );
+my $groupwork = $g->approach;
+my $leaguemembers = $league->members;
+my %members = map { $_->{id} => $_ } @$leaguemembers;
+my $session = $script->session;
+my $week = $script->round;
+my $letter = $script->player;
+my @tables = split /,/, $script->tables;
+
+my $texString = 
+'\documentclass[a4paper]{article}
+\newcommand{\printlandscape}{\special{landscape}}
+\printlandscape
+\usepackage[landscape]{geometry}
+\usepackage{multicol}
+\usepackage[usenames,dvipsnames]{color}
+\usepackage[absolute]{textpos}
+% \usepackage[absolute,showboxes]{textpos}
+% \textblockorigin{0.00cm}{+0.15cm} %HP DeskJet 5160
+\textblockorigin{0.00cm}{0.00cm} %HPDeskJet5160
+% \textblockorigin{0.00cm}{0.50cm} %HPDeskJet5160
+% \textblockorigin{0.15cm}{0.00cm} %HPDeskJet5160
+\pagestyle{empty}
+\setlength{\unitlength}{1cm}
+
+\newcommand{\mycard}[2]{%
+\TPMargin{0.0cm}
+	\begin{textblock}{4}(#1)
+% \textblockrulecolor{red}
+	\textblocklabel{picture1}
 	\vspace{0.25cm}
-	\normalsize II. \rule{1.0cm}{0.3pt} Champion: \rule{1.3cm}{0.3pt}\\
+\begin{minipage}{7.0cm}%
+	\raggedleft \large Conversation competition \hfill \normalsize \raisebox{-0.2cm}{Week: #3} \\
+	\vspace{-0.05cm}
+	\large I. #6 \\& II. #8 Groups\\\\
+	\raggedright
+	\normalsize Story: #4 \hfill \\
+	\vspace{-0.05cm}
+	\large Champions (Letter #5:\\
+	\vspace{0.09cm}
+	\normalsize I. #6 Champion: #7\\
+	\vspace{0.25cm}
+	\normalsize II. #8 Champion: #9\\
 	\vspace{0.25cm}
 	\large Winner: \rule{2.1cm}{0.3pt}\\
 	\large Votes:\\
@@ -39,13 +93,35 @@
 	\end{textblock}
 	\TPshowboxesfalse
 	}
-
 \begin{document}
 % \fontfamily{hlst}\fontseries{b}\fontshape{n}\selectfont
 
-\mycard{0,0}{2.8,1.0} \mycard{4,0}{6.8,1.0} \mycard{8,0}{10.8,1.0} \mycard{12,0}{14.8,1.0}
-\mycard{0,8}{2.8,9.0} \mycard{4,8}{6.8,9.0} \mycard{8,8}{10.8,9.0} \mycard{12,8}{14.8,9.0}
+';
 
+my @textpos = (
+	[ [0,0], [2.8,1.0]
+	, [ [4,0], [6.8,1.0]
+	, [ [8,0], [10.8,1.0]
+	, [ [12,0], [14.8,1.0]
+	, [ [0,8], [2.8,9.0]
+	, [ [4,8], [6.8,9.0]
+	, [ [8,8], [10.8,9.0]
+	, [ [12,8], [14.8,9.0]
+	);
+my $paging = 0;
+
+my $groups = $groupwork->beancans($session);
+my %indexed = ( A => 0, B => 1, C => 2, D => 3 );
+my
+foreach my $table ( @tables ) {
+	my @two = split /::/, $table;
+	my @champion = map { $groups->{$_}->%indexed{$letter} } @two;
+	$texString .=
+"\\mycard{ $textpos[$paging][0] }{ $textpos[$paging][1] }{$week}{$story}{$letter}{$two[0]}{$champion[0]}{$two[1]}{$champion[1]}
+";
+	paging;
+}
+$texString .= '
 			% crosshairs cutting guide marks
 			\begin{textblock}{1}(3.64,8)\rule{3mm}{0.1mm}%
 			\end{textblock}
@@ -72,118 +148,17 @@
 \null\newpage
 
 \end{document}
-
-#!/usr/bin/perl 
-
-use strict;
-use warnings;
-
-use YAML qw/LoadFile DumpFile/;
-use IO::All;
-
-use Getopt::Long;
-use Pod::Usage;
-
-my $man = 0;
-my $help = 0;
-my ($league, $session, $week, $letter, $tables);
-
-GetOptions (
-	"league=s" => \$league, "session=s" => \$session, "round=s" => \$week,
-	"player=s" => \$letter, "tables=s" => \$tables,
-	'help|?' => \$help, man => \$man) or pod2usage(2);
-pod2usage(1) if $help;
-pod2usage(-exitstatus => 0, -verbose => 2) if $man;
-
-my $league = LoadFile "/home/greg/$league/league.yaml"
-					or die "No register.yaml: $!";
-my @tables = split /,/, $tables;
-my $groups = LoadFile "/home/greg/$league/$series/teams.yaml";
-
-my @latex = (
-                { page => 1, xy => "0,0" },
-                { page => 1, xy => "8,0" },
-                { page => 1, xy => "0,4" },
-                { page => 1, xy => "8,4" },
-                { page => 1, xy => "0,8" },
-                { page => 1, xy => "8,8" },
-                { page => 1, xy => "0,12" },
-                { page => 1, xy => "8,12" },
-                { page => 2, xy => "0,0" },
-                { page => 2, xy => "8,0" },
-                { page => 2, xy => "0,4" },
-                { page => 2, xy => "8,4" },
-                { page => 2, xy => "0,8" },
-                { page => 2, xy => "8,8" },
-                { page => 2, xy => "0,12" },
-                { page => 2, xy => "8,12" },
-        );
-my $paging = 0;
-my $threepages = 0;
-
-
-my $texString = 
-'\documentclass[a4paper]{article}
-\newcommand{\printlandscape}{\special{landscape}}
-\printlandscape
-\usepackage[landscape]{geometry}
-\usepackage{multicol}
-\usepackage[usenames,dvipsnames]{color}
-\usepackage[absolute]{textpos}
-% \usepackage[absolute,showboxes]{textpos}
-% \textblockorigin{0.00cm}{+0.15cm} %HP DeskJet 5160
-\textblockorigin{0.00cm}{0.00cm} %HPDeskJet5160
-% \textblockorigin{0.00cm}{0.50cm} %HPDeskJet5160
-% \textblockorigin{0.15cm}{0.00cm} %HPDeskJet5160
-\pagestyle{empty}
-\setlength{\unitlength}{1cm}
-
-\newcommand{\mycard}[2]{%
-\TPMargin{0.0cm}
-	\begin{textblock}{4}(#1)
-% \textblockrulecolor{red}
-	\textblocklabel{picture1}
-	\vspace{0.25cm}
-\begin{minipage}{7.0cm}%
 ';
 
-$texString .= 
-"\raggedleft \large Conversation competition \hfill \normalsize \raisebox{-0.2cm}{Week: $week} \\
-\vspace{-0.05cm}
-";
-
-foreach my $team ( keys %$teams ) 
-{
-	$texString .= 
-"\\begin{textblock}{8}($latex[$paging]->{xy})
-\\textblocklabel{picture1}
-\\mycard{evalform}{$team}{$week}
-";
-	my $arguments = 2;
-	my $members = $teams->{$team};
-	foreach my $member ( @$members ) 
-	{
-		# my $name = $teams->{$team}->[$member];
-		$texString .= "{\\myeval{$member}}";
-		$arguments++;
-	}
-	$texString .= '{}' while $arguments++ < 5;
-	$texString .= "\n\\end{textblock}\n\n";
-	&paging;
-}
-
-$texString .= '\end{document}';
-
-$texString > io "evalform.tex";
+$texString > io "conversation.tex";
 
 sub paging
 {       if ($paging == 7 )
         {
-                $texString .= "
-\\begin{tiny}" . ($threepages + $latex[$paging]->{page}) . "\\end{tiny}\\newpage
-\n\n";
+                $paging = 0;
+		$texString .= '\null\newpage
+';
         }
-        if ($paging == 15) { $threepages = $threepages+3; $paging = 0; }
         else { $paging++; }
 }
 
@@ -191,7 +166,7 @@ __END__
 
 =head1 NAME
 
-conversation.pl - Create Conversation Competition forms for 2 groups' champions
+conversation.pl - Create Conversation Competition forms for 2 groups and champions
 
 =head1 SYNOPSIS
 
