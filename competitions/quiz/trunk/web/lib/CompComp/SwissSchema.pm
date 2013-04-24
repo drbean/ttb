f6264a0e9a0ca4db6ba8b9285807c3c5c57065a1
drbean <drbean@users.noreply.github.com>
2013-04-24 13:26:40 +0000

    Back out r7483 /home/drbean/swiss/web/lib

--- competitions/quiz/trunk/web/lib/CompComp/SwissSchema.pm
+++ competitions/quiz/trunk/web/lib/CompComp/SwissSchema.pm
@@ -5,8 +5,6 @@ use warnings;
 use FindBin qw/$Bin/;
 
 use lib "$Bin/../../../swiss/web/lib";
-use lib "/var/www/cgi-bin/swiss/lib";
-use lib "/home/drbean/swiss/web/lib";
 
 use base 'DBIx::Class::Schema';
 
