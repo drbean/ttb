09f9a0b15ae5473756a10b8d73b71992d25af494
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2015-10-31 04:59:35 +0000

    dic^Dic.up_1_directory_with_comp/web

--- competitions/quiz/trunk/web/lib/CompComp/dicSchema.pm
+++ competitions/quiz/trunk/web/lib/CompComp/dicSchema.pm
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 use FindBin qw/$Bin/;
 
-use lib "$Bin/../../dic/lib";
+use lib "$Bin/../../../dic/lib";
 
 use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces(
-	result_namespace => '+dic::Schema'
+	result_namespace => '+Dic::Schema'
 	);
 
 
