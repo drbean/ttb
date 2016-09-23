3656d0b88e7d588f37ea81a33891eab4d5ff85ed
drbean <drbean@users.noreply.github.com>
2016-09-23 04:27:20 +0000

    fourhand:c=cloze_style=firstlast.TODO.why_3rd_letter_of_3-letter_words_clozed?

--- dictation/lib/Dic/Cloze/Fourhand.pm
+++ dictation/lib/Dic/Cloze/Fourhand.pm
@@ -1,6 +1,6 @@
 package Dic::Cloze::Fourhand;  # assumes Some/Module.pm
 
-# Last Edit: 2016 Sep 20, 01:48:31 PM
+# Last Edit: 2016 Sep 23, 11:47:24 AM
 # $Id: /cloze/branches/ctest/Cloze.pm 1234 2007-06-03T00:32:38.953757Z greg  $
 
 use strict;
@@ -23,6 +23,7 @@ use Parse::RecDescent;
 sub cloze
 {
 	$::RD_HINT=1;
+	my $cloze_style = shift;
 	my $unclozeables = shift;
 	chomp $unclozeables;
 	our $unclozeable = $unclozeables? qr/(?:$unclozeables)/: undef;
@@ -59,6 +60,12 @@ $DB::single=1;
 			}
 			string: token(s) end | <error>
 			token: pass | firstletter | middleletter | lastletter | punctuation
+			punctuation: <reject: $inWord> m/$punctuation/
+				{
+					$Dic::Cloze::Fourhand::clozeline{$_} .= $item[2] for @Dic::Cloze::Fourhand::writers;
+					$Dic::Cloze::Fourhand::clozeline{$Dic::Cloze::Fourhand::reader} .= $item[2];
+				}
+			end: m/^\Z/
 			firstletter: <reject: $inWord> m/[A-Za-z0-9]/ 
 				{ $inWord=1; $index = 0; @cword = ();
 					$Dic::Cloze::Fourhand::word_score++;
@@ -73,6 +80,9 @@ $DB::single=1;
 					# $Cloze::clozeline{$writer} .= "\\\\1{}";
 					push @cword, $item[2];
 				}
+		]; 
+		if ( $cloze_style ne 'firstlast' ) {
+			$letterGrammar .= q[
 			lastletter: <reject: not $inWord> m/$letter(?=$punctuation)/
 				{
 					$inWord=0;
@@ -90,13 +100,50 @@ $DB::single=1;
 					}
 					$Dic::Cloze::Fourhand::clozeline{$Dic::Cloze::Fourhand::reader} .= join '', @cword, "\\\\hspace{0.05cm}", "\\\\textsubscript{\\\\tiny $Dic::Cloze::Fourhand::word_score}";
 				}
-			punctuation: <reject: $inWord> m/$punctuation/
+			]; 
+		}
+		if ( $cloze_style eq 'firstlast' ) {
+			$letterGrammar .= q[
+			lastletter: <reject: not $inWord> m/$letter(?=$punctuation)/
 				{
-					$Dic::Cloze::Fourhand::clozeline{$_} .= $item[2] for @Dic::Cloze::Fourhand::writers;
-					$Dic::Cloze::Fourhand::clozeline{$Dic::Cloze::Fourhand::reader} .= $item[2];
+					$inWord=0;
+					$index++;
+					$Dic::Cloze::Fourhand::letter_score++;
+					# $Cloze::clozeline{$writer} .= "\\\\1{}";
+					push @cword, $item[2];
+					if ( $#cword > 2 ) {
+						$Dic::Cloze::Fourhand::clozeline{$_} .= join '', (@cword[0], "\\\\1{$Dic::Cloze::Fourhand::word_score}"
+							, map {"\\\\1{}"} 0 .. ( $#cword - 3 ) )
+							, $cword[-1] for @Dic::Cloze::Fourhand::writers;
+					}
+					else {
+						$Dic::Cloze::Fourhand::clozeline{$_} .= join '', (@cword[0..$#cword/2], "\\\\1{$Dic::Cloze::Fourhand::word_score}" , map {"\\\\1{}"} reverse 1 .. $#cword-($#cword-1)/2-1) for @Dic::Cloze::Fourhand::writers;
+					}
+					$Dic::Cloze::Fourhand::clozeline{$Dic::Cloze::Fourhand::reader} .= join '', @cword, "\\\\hspace{0.05cm}", "\\\\textsubscript{\\\\tiny $Dic::Cloze::Fourhand::word_score}";
 				}
-			end: m/^\Z/
-		]; 
+			]; 
+		}
+		else {
+			$letterGrammar .= q[
+			lastletter: <reject: not $inWord> m/$letter(?=$punctuation)/
+				{
+					$inWord=0;
+					$index++;
+					$Dic::Cloze::Fourhand::letter_score++;
+					# $Cloze::clozeline{$writer} .= "\\\\1{}";
+					push @cword, $item[2];
+					if ( $#cword > 2 ) {
+						$Dic::Cloze::Fourhand::clozeline{$_} .= join '', (@cword[0..$#cword/2], "\\\\1{$Dic::Cloze::Fourhand::word_score}"
+							, map {"\\\\1{}"} reverse 2 .. $#cword-($#cword-1)/2-1)
+							, $cword[-1] for @Dic::Cloze::Fourhand::writers;
+					}
+					else {
+						$Dic::Cloze::Fourhand::clozeline{$_} .= join '', (@cword[0..$#cword/2], "\\\\1{$Dic::Cloze::Fourhand::word_score}" , map {"\\\\1{}"} reverse 1 .. $#cword-($#cword-1)/2-1) for @Dic::Cloze::Fourhand::writers;
+					}
+					$Dic::Cloze::Fourhand::clozeline{$Dic::Cloze::Fourhand::reader} .= join '', @cword, "\\\\hspace{0.05cm}", "\\\\textsubscript{\\\\tiny $Dic::Cloze::Fourhand::word_score}";
+				}
+			];
+		} 
 		if ( $unclozeables ) {
 			$letterGrammar .= q[
 			pass: <reject: $inWord> m/($Dic::Cloze::Fourhand::unclozeable|\w)(?=$punctuation)/m
