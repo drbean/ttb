2b5d37f06d3d25fc2b9df91a79d2ebbb84f7c53c
drbean <drbean@users.noreply.github.com>
2016-10-10 02:56:30 +0000

    lib/Dic_not_added!

---
+++ dictation/lib/Dic.pm
@@ -0,0 +1,84 @@
+package Dic;
+
+# Created: 06/25/2016 10:28:33 AM
+# Last Edit: 2016 Jun 26, 02:14:46 PM
+# $Id$
+
+=head1 NAME
+
+Dic.pm - base lib for running dic
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+use App::Cmd::Setup -app;
+
+=head1 DESCRIPTION
+
+Base class for a collection of different clozing styles for a series of dictation apps.
+
+=cut
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-/home/drbean/class/ttb/dictation/libDic.pm at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=/home/drbean/class/ttb/dictation/libDic.pm>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+	perldoc /home/drbean/class/ttb/dictation/libDic.pm
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist//home/drbean/class/ttb/dictation/libDic.pm>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d//home/drbean/class/ttb/dictation/libDic.pm>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=/home/drbean/class/ttb/dictation/libDic.pm>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist//home/drbean/class/ttb/dictation/libDic.pm>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2016 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;    # End of /home/drbean/class/ttb/dictation/libDic.pm
+
+# vim: set ts=8 sts=4 sw=4 noet:
+
