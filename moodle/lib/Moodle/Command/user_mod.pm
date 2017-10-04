5a4b047bb2a3f1749807c1355e1b7fdb0f34fac3
drbean <drbean@freeshell.org>
2017-10-04 14:37:33 +0800

    from_user-mod.rc.following_user_create,group_build.broken!

---
+++ moodle/lib/Moodle/Command/user_mod.pm
@@ -0,0 +1,75 @@
+package Moodle::Command::user_mod;
+
+use lib "lib";
+
+use Moodle -command;
+use strict;
+use warnings;
+use YAML qw/Dump LoadFile DumpFile/;
+use IO::All;
+
+sub abstract { "Leverage Moosh's php scripts to do higher-level moodle work in perl" }
+sub description { "Leverage Moosh's php scripts to do higher-level moodle work in perl" }
+
+sub usage_desc { "moodle user_mod -s nuu -l FLA0003" }
+
+sub opt_spec  {
+        return (
+                ["l=s", "league"]
+	);
+}
+
+
+sub execute {
+	my ($self, $opt, $args) = @_;
+
+	my ($league) = @$opt{qw/l/};
+	my $semester="$ENV{SEMESTER}";
+
+	chdir '/var/www/cgi-bin/moodle';
+
+	my @course_info = qx/Moosh course-list -n "shortname='$league'"/;
+	my $course_id_line = chomp $course_info[1];
+	(my $course_id = $course_id_line) =~ tr/"//;
+
+
+	use Grades;
+	my $l = League->new( leagues => "/home/drbean/$semester", id => $league );
+	my %m = map { $_->{id} => $_ } @{ $l->members };
+	#my %m = ( drbean =>
+	#	{ Chinese => "drbean",
+	#	id => "drbean",
+	#	name => "DrBean",
+	#	password => "bean",
+	#	}
+	#);
+	my $city;
+	if ( $l->yaml->{school} eq "must" ) {
+		$city = "Hsinchu";
+	}
+	else { $city = "Miaoli";}
+
+	my $user_list = qx/Moosh user-list --course $course_id/;
+	my @user_list = split /\n/, $user_list;
+	my @users;
+	for my $user ( @user_list ) {
+		$user =~ m/^([a-z]\d*) \((\d*)\)/;
+		next unless $1 && $2;
+		push @users, {username => $1, id => $2};
+	}
+
+	my @mod;
+	for my $user ( @users ) {
+		my $id = $user->{id};
+		my $schoolid = ucfirst $user->{username};
+		my $password = $m{$schoolid}->{password};
+		my $firstname = $m{$schoolid}->{name};
+		my $email = $m{$schoolid}->{email};
+		my $city = $city;
+		system("Moosh user-mod -i --firstname $firstname --password $password --email $email $id");
+		push @mod, {firstname => $firstname, id => $id};
+	}
+	print "firstname: " . $_->{firstname} . "\tid: " . $_->{id} . "\n" for @mod;
+}
+
+1;
