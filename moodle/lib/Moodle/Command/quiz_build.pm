085ad0d85999396d98b837f89ad580c3e9c5289f
drbean <drbean@freeshell.org>
2018-02-20 21:17:22 +0900

    quiz_for_jigsaw

---
+++ moodle/lib/Moodle/Command/quiz_build.pm
@@ -0,0 +1,63 @@
+package Moodle::Command::jigsaw_build;
+
+use lib "lib";
+
+use Moodle -command;
+use strict;
+use warnings;
+use YAML qw/Dump LoadFile DumpFile/;
+use IO::All;
+
+sub abstract { "moopl jigsaw_build -l 2L1b -c conversation -t story -s gold -f 0 -x 5 -g 45" }
+sub description { "Build jigsaw for course, session 5, grouping id 45" }
+
+sub usage_desc { "moodle jigsaw_build -l BMA0031 -c 23" }
+
+sub opt_spec  {
+        return (
+                ["l=s", "league"]
+				, ["c=s", "course"]
+				, ["t=s", "topic"]
+				, ["s=s", "story"]
+				, ["f=s", "form"]
+				, ["g=s", "grouping"]
+				, ["x=s", "section"]
+	);
+}
+
+
+sub execute {
+	my ($self, $opt, $args) = @_;
+
+	my ($league, $course, $topic, $story, $form, $grouping, $section) = @$opt{qw/l c t s f g x/};
+	my $semester="$ENV{SEMESTER}";
+
+	chdir "/var/www/cgi-bin/moodle";
+
+	my $course_id = qx/Moosh course-list -i "shortname='$league'"/;
+	chomp $course_id;
+	my $expert_groups = qx/Moosh group-list -G $grouping $course_id/;
+	my @expert_groups = split /\n/, $expert_groups;
+
+	use Grades;
+	my $l = League->new( leagues => "/home/drbean/$semester", id => $league );
+	my $yaml = LoadFile "/home/drbean/curriculum/$course/$topic/cards.yaml";
+	my $cards = $yaml->{$story}->{jigsaw}->{$form};
+	my @roles = qw/A B C/;
+	my %role_cards;
+	@role_cards{@roles}= @$cards{@roles};
+	my $quiz_cards = $cards->{quiz};
+
+	print "cards: $story, $form\n";
+	for my $group ( @expert_groups ) {
+		next unless $group =~ m/\bgroup\b/;
+		( my $id = $group ) =~ s/^[\D]*(\d+).*$/$1/;
+		( my $role = $group ) =~ s/^.*"\d+-([ABC]).*$/$1/;
+		my $json = q/{\"op\":\"&\",\"c\":[{\"type\":\"group\",\"id\":/ . $id . q/}],\"showc\":[false]}/;
+		system("Moosh activity-add -n $story$form -c \"$role_cards{$role}\" -s $section -a \"$json\" page $course_id");
+	}
+	# system("moopl group_build -l $league");
+
+}
+
+1;
