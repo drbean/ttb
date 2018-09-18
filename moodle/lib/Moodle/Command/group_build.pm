f2b4cbf88d4b5de00b0fd6face44cf6e6f3e664b
drbean <drbean@freeshell.org>
2018-09-18 15:43:39 +0800

    s/this//session

--- moodle/lib/Moodle/Command/group_build.pm
+++ moodle/lib/Moodle/Command/group_build.pm
@@ -40,8 +40,8 @@ sub execute {
 	my $l = League->new( leagues => "/home/drbean/$semester", id => $league );
 	my $g = Grades->new({ league => $l });
 	my $cl = $g->classwork;
-	my $thissession = $cl->lastsession;
-	my $beancans = $cl->beancan_names($thissession);
+	my $session = $cl->lastsession;
+	my $beancans = $cl->beancan_names($session);
 	my @groups = sort keys %$beancans;
 	print "league: $league\n";
 	$" = " ";
