c644d5527f9df1350adb51a5380950adc02801fe
drbean <drbean@freeshell.org>
2019-01-15 13:27:35 +0800

    hardcode:category=4755,quiz=2281.quiz=s/format/type/

--- moodle/lib/Moodle/Command/question_import.pm
+++ moodle/lib/Moodle/Command/question_import.pm
@@ -29,31 +29,32 @@ sub execute {
 	my $semester="$ENV{SEMESTER}";
 
 	chdir "/var/www/cgi-bin/moodle";
-
-	# Save questions in ${exam}_exam_$semester category for quiz
-	my $category = qx|~/dot/postgres/db/script/db moodle -p 5433 -d mood071 -u postgres  -t question_categories -a select -k name  -v final_exam_071 -s id|;
-	my @category = split /\n/, $category;
-	die "${exam}_exam_$semester category id is which @category category?\n"
-		unless @category == 1;
-	my $id = shift @category;
-	chomp $id;
-	die "${exam}_exam_$semester category id is $id?\n"
-		unless looks_like_number($id);
-
-	# Get exam quiz activity id
-	my $quiz = qx/Moosh -n sql-run "SELECT id FROM {quiz} WHERE name =  \'${exam}_exam_$semester\'"/;
-	chomp $quiz;
-	die "${exam}_exam_$semester quiz id is $quiz?\n"
+	## Save questions in ${exam}_exam_$semester category for quiz
+	#my $category = qx|~/dot/postgres/db/script/db moodle -p 5433 -d mood071 -u postgres  -t question_categories -a select -k name  -v final_exam_071 -s id|;
+	#my @category = split /\n/, $category;
+	#die "${exam}_exam_$semester category id is which @category category?\n"
+	#	unless @category == 1;
+	#my $id = shift @category;
+	#chomp $id;
+	my $category = 4755;
+	die "${exam}_exam_$semester category id is $category?\n"
 		unless looks_like_number($category);
 
-	my $yaml = LoadFile "/home/drbean/curriclum/correspondence/exam/question.yaml";
+	## Get exam quiz activity id
+	#my $quiz = qx/Moosh -n sql-run "SELECT id FROM {quiz} WHERE name =  \'${exam}_exam_$semester\'"/;
+	#chomp $quiz;
+	my $quiz= 2281;
+	die "${exam}_exam_$semester quiz id is $quiz?\n"
+		unless looks_like_number($quiz);
+
+	my $yaml = LoadFile "/home/drbean/curriculum/correspondence/exam/question.yaml";
 	my $question_list = $yaml->{$semester}->{$exam};
-	my $n=0;
+	die "list of questions: $question_list\n" unless ref( $question_list) eq "ARRAY" and $question_list;
 	for my $question ( @$question_list ) {
-		my ( $topic, $story, $format, $form ) = 
-			map { $question->{$_} } qw/topic story format form/;
-		my $file = "/var/lib/moodle/repository/$topic/quiz_${story}_${format}_$form.xml";
-		die "No $story ($format) $form form file in repository/$topic?" unless
+		my ( $topic, $story, $type, $form ) = 
+			map { $question->{$_} } qw/topic story type form/;
+		my $file = "/var/lib/moodle/repository/$topic/quiz_${story}_${type}_$form.xml";
+		die "No $story ($type) $form form file in repository/$topic?" unless
 			-e $file;
 		qx/Moosh -n question-import $file $quiz $category/;
 	}
