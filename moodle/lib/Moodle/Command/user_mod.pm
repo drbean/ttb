ed6e8a87eea4900bda757bde7cad4cfc38fe49d3
drbean <drbean@freeshell.org>
2019-09-14 22:11:10 +0800

    do_not_reset_email_address.respect_user_set_to_self-chosen_address

--- moodle/lib/Moodle/Command/user_mod.pm
+++ moodle/lib/Moodle/Command/user_mod.pm
@@ -73,7 +73,7 @@ sub execute {
 		my $firstname = '"' . $m{$schoolid}->{name} . '"';
 		my $email = $m{$schoolid}->{email};
 		my $city = $city;
-		system("Moosh -n user-mod -i --idnumber $schoolid --firstname $firstname --email $email $id");
+		system("Moosh -n user-mod -i --idnumber $schoolid --firstname $firstname $id");
 		push @mod, {firstname => $firstname, id => $id, idnumber => $schoolid};
 	}
 	print "firstname: " . $_->{firstname} . "\tid: " . $_->{id} . "\tidnumber: " . $_->{idnumber} . "\n" for @mod;
