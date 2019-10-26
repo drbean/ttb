cad2e222cbf8126046149819a593b03c5bf011d1
drbean <drbean@freeshell.org>
2019-10-26 14:50:27 +0800

    print_forum_id,quiz_id_so_easier_to_activity-delete!

--- moodle/lib/Moodle/Command/section_populate.pm
+++ moodle/lib/Moodle/Command/section_populate.pm
@@ -49,6 +49,7 @@ sub execute {
 		if ( $type eq 'forum' ) {
 			my $name = $first_one->{intro};
 			my $forum_id = qx(/home/drbean/moodle/moosh/moosh.php -n activity-add -n '$name' -s $section -o "--timeopen=1 --intro=$(IFS= cat /home/drbean/curriculum/$course_name/$story/intro.txt) --introformat=4 --type=eachuser  --grade=3 --gradecat=$gradecat --decimalpoints=0" forum $course);
+			warn "forum_id=$forum_id";
 			next;
 		}
 		my $cards = "/home/drbean/curriculum/$course_name/$topic/cards.yaml";
@@ -59,6 +60,7 @@ sub execute {
 		$intro = "$topic: $story $form" unless $intro;
 		# my $quiz_id = qx(/home/drbean/moodle/moosh/moosh.php -n activity-add -n '$name' -s $section -o="--timeopen=1 --intro=$intro --introformat=4 --grade=3 --gradecat=$gradecat --groupmode=1 --groupingid=127 --attempts=1 --decimalpoints=0 --overduehandling=0 --shuffleanswers=1 --subnet=210.60.168.212 --browsersecurity=safebrowser --safeexambrowser_allowedkeys=ce1d2c15a11c5ae861c97e8fadc7e88d8edee4c66290693d8049f68e5c68070d" quiz $course);
 		my $quiz_id = qx(/home/drbean/moodle/moosh/moosh.php -n activity-add -n '$name' -s $section -o="--timeopen=1 --intro=$intro --introformat=4 --grade=3 --gradecat=$gradecat --groupmode=1 --groupingid=127 --attempts=1 --decimalpoints=0 --overduehandling=0 --shuffleanswers=1" quiz $course);
+		warn "quiz_id=$quiz_id";
 		chomp $quiz_id;
 		die "Failed to add '$name' activity to section $section with activity-add! activity_id=$quiz_id\n" unless looks_like_number( $quiz_id );
 		for my $question ( @$question_list ) {
