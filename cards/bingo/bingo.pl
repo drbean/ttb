c287cb0ac2cbe0f42dba92a0ea85ed234e935ee3
drbean <drbean@freeshell.org>
2021-02-23 15:45:13 +0800

    match_like_correspondence,ref=HASH,exists.s/word,call/pair/,plus_massive_indentation.s/tab/spaces/?

--- cards/bingo/bingo.pl
+++ cards/bingo/bingo.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Last Edit: 2020 Feb 11, 09:47:45 PM
+# Last Edit: 2021 Feb 18, 12:48:55 PM
 # $Id: /dic/branches/ctest/dic.pl 1263 2007-06-23T12:37:20.810966Z greg  $
 
 use strict;
@@ -20,9 +20,9 @@ my $s = '';
 my $f = 0;
 
 GetOptions (
-	'help|?' => \$help, man => \$man,
-	'n=i' => \$n, 's=s' => \$s, 'f=i' => \$f)
-		or pod2usage(2);
+        'help|?' => \$help, man => \$man,
+        'n=i' => \$n, 's=s' => \$s, 'f=i' => \$f)
+                or pod2usage(2);
 pod2usage(1) if $help;
 pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
@@ -30,9 +30,9 @@ use IO::All;
 use YAML qw/LoadFile DumpFile/;
 
 my %romanize = (
-	0 => "Zero", 1 => "One", 2 => "Two", 3 =>"Three"
-	, 4 => "Four", 5 => "Five", 6 => "Six", 7 =>"Seven"
-	, 8 => "Eight", 9 => "Nine", 10 => "Ten", 11 =>"Eleven" 
+        0 => "Zero", 1 => "One", 2 => "Two", 3 =>"Three"
+        , 4 => "Four", 5 => "Five", 6 => "Six", 7 =>"Seven"
+        , 8 => "Eight", 9 => "Nine", 10 => "Ten", 11 =>"Eleven" 
 );
 
 my $latexString = <<"START_LATEX";
@@ -43,7 +43,7 @@ my $latexString = <<"START_LATEX";
 \\setmainfont{Linux Libertine O}[Scale=MatchLowercase]
 \\usepackage{xeCJK}
 \\setCJKmainfont{NotoSansCJK-Regular.ttc}[
-	Path = /usr/share/fonts/noto/,
+        Path = /usr/share/fonts/noto/,
 ]
 % \\usepackage[absolute,noshowtext,showboxes]{textpos}
 \\usepackage[absolute,showboxes]{textpos}
@@ -61,43 +61,43 @@ my $latexString = <<"START_LATEX";
 \\NumTabs{4}
 
 \\newcommand{\\bingoX${s}X$romanize{$f}Xcard}[5]{%
-	\\vspace{0.1cm}
-	\\small #1 #2
-	\\par
-	\\parbox[t][6.7cm][c]{9.5cm}{%
-	\\hspace{0.1cm} \\Large#3\\\\
-	\\normalsize#4 #5
-	}
+        \\vspace{0.1cm}
+        \\small #1 #2
+        \\par
+        \\parbox[t][6.7cm][c]{9.5cm}{%
+        \\hspace{0.1cm} \\Large#3\\\\
+        \\normalsize#4 #5
+        }
 }
 
 START_LATEX
 
 my @latex = (
-		{ page => 1, xy => "0,0" },
-		{ page => 1, xy => "0,4" },
-		{ page => 1, xy => "8,0" },
-		{ page => 1, xy => "8,4" },
-		{ page => 1, xy => "0,8" },
-		{ page => 1, xy => "0,12" },
-		{ page => 1, xy => "8,8" },
-		{ page => 1, xy => "8,12" },
-		{ page => 2, xy => "0,0" },
-		{ page => 2, xy => "0,4" },
-		{ page => 2, xy => "8,0" },
-		{ page => 2, xy => "8,4" },
-		{ page => 2, xy => "0,8" },
-		{ page => 2, xy => "0,12" },
-		{ page => 2, xy => "8,8" },
-		{ page => 2, xy => "8,12" },
-		{ page => 3, xy => "0,0" },
-		{ page => 3, xy => "0,4" },
-		{ page => 3, xy => "8,0" },
-		{ page => 3, xy => "8,4" },
-		{ page => 3, xy => "0,8" },
-		{ page => 3, xy => "0,12" },
-		{ page => 3, xy => "8,8" },
-		{ page => 3, xy => "8,12" },
-	);
+                { page => 1, xy => "0,0" },
+                { page => 1, xy => "0,4" },
+                { page => 1, xy => "8,0" },
+                { page => 1, xy => "8,4" },
+                { page => 1, xy => "0,8" },
+                { page => 1, xy => "0,12" },
+                { page => 1, xy => "8,8" },
+                { page => 1, xy => "8,12" },
+                { page => 2, xy => "0,0" },
+                { page => 2, xy => "0,4" },
+                { page => 2, xy => "8,0" },
+                { page => 2, xy => "8,4" },
+                { page => 2, xy => "0,8" },
+                { page => 2, xy => "0,12" },
+                { page => 2, xy => "8,8" },
+                { page => 2, xy => "8,12" },
+                { page => 3, xy => "0,0" },
+                { page => 3, xy => "0,4" },
+                { page => 3, xy => "8,0" },
+                { page => 3, xy => "8,4" },
+                { page => 3, xy => "0,8" },
+                { page => 3, xy => "0,12" },
+                { page => 3, xy => "8,8" },
+                { page => 3, xy => "8,12" },
+        );
 my $paging = 0;
 my $threepages = 0;
 
@@ -110,11 +110,14 @@ my $identifier = "$s $f";
 $identifier =~ s/_/ /;
 $latexString .= "\\newcommand{\\bingoX${s}X$romanize{$f}XIdentifier}[0]{$identifier\n}\n\n";
 my $bingo;
-if (exists $story->{bingo} && exists $story->{bingo}->[$f] ) {
-	$bingo = $story->{bingo}->[$f];
+if (exists $story->{match} && exists $story->{match}->[$f] ) {
+        $bingo = $story->{match}->[$f];
+}
+elsif (exists $story->{bingo} && exists $story->{bingo}->[$f] ) {
+        $bingo = $story->{bingo}->[$f];
 }
 elsif (exists $story->{$f} and exists $story->{$f}->{bingo} ) {
-	$bingo = $story->{$f}->{bingo};
+        $bingo = $story->{$f}->{bingo};
 
 }
 else { die "No bingo for $s story, form $f" }
@@ -122,7 +125,16 @@ else { die "No bingo for $s story, form $f" }
 $latexString .= "\\begin{document}\n\n";
 
 my (@words, %prompts);
-if ( ref $bingo eq 'HASH' and exists $bingo->{word} and exists $bingo->{call} ) {
+if ( ref $bingo eq 'HASH' and exists $bingo->{pair} ) {
+        my $pair = $bingo->{pair};
+       push @words, $pair->[$_]->[1] for (0..$#$pair);
+       my @prompts;
+       push @prompts, $pair->[$_]->[0] for (0..$#$pair);
+       @prompts{@words} = @prompts;
+       die "Unequal word, call numbers. Also check order"
+               unless ( @words == @prompts );
+}
+elsif ( ref $bingo eq 'HASH' and exists $bingo->{word} and exists $bingo->{call} ) {
        @words = split m/ /, $bingo->{word};
        my @prompts = @{ $bingo->{call} };
        @prompts{@words} = @prompts;
@@ -130,53 +142,53 @@ if ( ref $bingo eq 'HASH' and exists $bingo->{word} and exists $bingo->{call} ) 
                unless ( @words == @prompts );
 }
 elsif ( ref $bingo eq 'ARRAY' ) {
-	my $n;
-	for my $prompt ( @$bingo ) {
-		(my $word = $prompt ) =~ s/^[^_]*_(.*)_.*$/$1/;
-		push @words, $word;
-		die "No $word word in ${n}th, \"$prompt\" prompt"
-			unless $word;
-		$n++;
-		$prompts{$word} = $prompt;
-	}
+        my $n;
+        for my $prompt ( @$bingo ) {
+                (my $word = $prompt ) =~ s/^[^_]*_(.*)_.*$/$1/;
+                push @words, $word;
+                die "No $word word in ${n}th, \"$prompt\" prompt"
+                        unless $word;
+                $n++;
+                $prompts{$word} = $prompt;
+        }
 }
 else {
-	@words = split m/ /, $bingo;
-	@prompts{@words} = @words;
+        @words = split m/ /, $bingo;
+        @prompts{@words} = @words;
 }
 
 my (%word_count, %part_count);
 $word_count{$_}++ for @words;
 for my $word ( keys %prompts ) {
-	for my $other ( keys %prompts ) {
-		next if $word eq $other;
-		my $prompt = $prompts{$other};
-		my @noise = split /[-,._\s]/, $prompt;
-		my @lc_noise = map (lc, @noise);
-		my @parts = split m/[_\s]/, $word;
-		my @lc_parts = map (lc, @parts);
-		for my $part ( @lc_parts ) {
-			for my $dupe ( @lc_noise ) {
-				$part_count{$part}++ if 
-					(($dupe eq $part)
-						# or ($dupe =~ m/$part/)
-						# or ($part =~ m/$dupe/)
-			);
-			}
-		}
-	}
+        for my $other ( keys %prompts ) {
+                next if $word eq $other;
+                my $prompt = $prompts{$other};
+                my @noise = split /[-,._\s]/, $prompt;
+                my @lc_noise = map (lc, @noise);
+                my @parts = split m/[_\s]/, $word;
+                my @lc_parts = map (lc, @parts);
+                for my $part ( @lc_parts ) {
+                        for my $dupe ( @lc_noise ) {
+                                $part_count{$part}++ if 
+                                        (($dupe eq $part)
+                                                # or ($dupe =~ m/$part/)
+                                                # or ($part =~ m/$dupe/)
+                        );
+                        }
+                }
+        }
 }
 for my $word ( @words ) {
-	die "calling '$word' word $word_count{$word} times"
-		unless $word_count{$word} == 1;
+        die "calling '$word' word $word_count{$word} times"
+                unless $word_count{$word} == 1;
 }
 #for my $part ( keys %part_count ) {
-#	die "'$part' dupe present $part_count{$part} times"
-#		unless $part_count{$part} == 0;
+#       die "'$part' dupe present $part_count{$part} times"
+#               unless $part_count{$part} == 0;
 #}
 die "No word for some prompts" unless
-	values %prompts == scalar @words;
-	
+        values %prompts == scalar @words;
+        
 warn "There are " . (sum values %word_count ) . " words\n";
 my @clinchers = sample( set => \@words, sample_size => 2 );
 my @winner = sample( set => \@clinchers );
@@ -185,21 +197,21 @@ my %words; @words{ @words } = (); delete @words{@clinchers};
 my @pruned = keys %words;
 my (@call, @lost_call);
 if ( ref $bingo eq 'HASH' and exists $bingo->{call} ) {
-	push @call, "$_: $prompts{$_}" for (@pruned, @winner);
-	push @lost_call, "$_: $prompts{$_}" for (@loser);
+        push @call, "$_: $prompts{$_}" for (@pruned, @winner);
+        push @lost_call, "$_: $prompts{$_}" for (@loser);
 
 }
 else {
-	@call = @prompts{ @pruned, @winner };
-	@lost_call = @prompts{@loser};
+        @call = @prompts{ @pruned, @winner };
+        @lost_call = @prompts{@loser};
 }
 
 my $h2p = Lingua::Han::PinYin->new( tone => 1 );
 
 sub translit {
-	my $chinese = shift ;
-	# return $chinese . "[" . $h2p->han2pinyin( $chinese ) . "]";
-	return $chinese;
+        my $chinese = shift ;
+        # return $chinese . "[" . $h2p->han2pinyin( $chinese ) . "]";
+        return $chinese;
 }
 
 my $regex = qr/([\N{U+4E00}-\N{U+9FFF}]+)/;
@@ -218,29 +230,29 @@ $latexString .= "}}{} \n \\end{textblock}\n \\TPshowboxesfalse \n";
 &paging;
 
 for my $card ( 0 .. $n-1 ) {
-	my @candidate = sample( set => \@clinchers );
-	my @presented = sample( set => \@pruned, sample_size => @pruned/2);
-	my ( @ordered, $it );
-	if  ( $card % 2 == 0 ) {
-		@ordered = sort {$a cmp $b} (@presented, @candidate);
+        my @candidate = sample( set => \@clinchers );
+        my @presented = sample( set => \@pruned, sample_size => @pruned/2);
+        my ( @ordered, $it );
+        if  ( $card % 2 == 0 ) {
+                @ordered = sort {$a cmp $b} (@presented, @candidate);
 
-	}
-	else {
-		@ordered = sort {$b cmp $a} (@presented, @candidate);
-	}
-	$it = natatime 3, @ordered;
+        }
+        else {
+                @ordered = sort {$b cmp $a} (@presented, @candidate);
+        }
+        $it = natatime 3, @ordered;
 
-	$latexString .= 
+        $latexString .= 
 "\\TPshowboxestrue
 \\begin{textblock}{8}($latex[$paging]->{xy})
 \\textblocklabel{picture$latex[$paging]->{xy}}
 \\bingoX${s}X$romanize{$f}Xcard{}{\\bingoX${s}X$romanize{$f}XIdentifier}{\\parbox{9.0cm}{";
-	while ( my @word = $it->() ) {
-		tr/_/~/ for @word;
-		$latexString .= "- $word[0] \\\\ - $word[1] \\\\ - $word[2] \\hspace{0pt}\\\\";
-	}
-	$latexString .= "}}{}{} \n \\end{textblock}\n \\TPshowboxesfalse \n";
-	&paging;
+        while ( my @word = $it->() ) {
+                tr/_/~/ for @word;
+                $latexString .= "- $word[0] \\\\ - $word[1] \\\\ - $word[2] \\hspace{0pt}\\\\";
+        }
+        $latexString .= "}}{}{} \n \\end{textblock}\n \\TPshowboxesfalse \n";
+        &paging;
 }
 $latexString .= "\\end{document}\n";
 
