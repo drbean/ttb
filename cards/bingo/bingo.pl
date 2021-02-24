8bd1bb890049b24cd927468965f9a645c411aa6f
drbean <drbean@freeshell.org>
2021-02-24 12:53:08 +0800

    match.form.s/array/hash,eg_0/

--- cards/bingo/bingo.pl
+++ cards/bingo/bingo.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Last Edit: 2021 Feb 24, 12:50:47 PM
+# Last Edit: 2021 Feb 24, 12:52:54 PM
 # $Id: /dic/branches/ctest/dic.pl 1263 2007-06-23T12:37:20.810966Z greg  $
 
 use strict;
@@ -112,7 +112,7 @@ $identifier =~ s/_/ /;
 $latexString .= "\\newcommand{\\bingoX${s}X$romanize{$f}XIdentifier}[0]{$identifier\n}\n\n";
 my $bingo;
 if (exists $story->{match} && exists $story->{match}->{$f} ) {
-        $bingo = $story->{match}->[$f];
+        $bingo = $story->{match}->{$f};
 }
 elsif (exists $story->{bingo} && exists $story->{bingo}->[$f] ) {
         $bingo = $story->{bingo}->[$f];
