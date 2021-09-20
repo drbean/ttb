bb234c9a756fca62f69ecc0d010c41c902e9ec0b
drbean <drbean@freeshell.org>
2021-09-20 20:31:35 +0800

    took_2933:2949_changes_from_text_dic.pl,but_will_not_run!

--- dictation/script/conversation_dic.pl
+++ dictation/script/conversation_dic.pl
@@ -1,26 +1,13 @@
-package Dic::Command::conversation;
+#!/usr/bin/env perl
 
-<<<<<<< .working
-# Last Edit: 2020 Jul 16, 04:21:58 PM
-||||||| .merge-left.r2933
-# Last Edit: 2021 Aug 08,  2:32:03 PM
-=======
-# Last Edit: 2021 Aug 18,  2:43:43 PM
->>>>>>> .merge-right.r2949
+# Last Edit: 2021 Sep 20,  8:29:58 PM
 # $Id: /cloze/branches/ctest/dic.pl 1134 2007-03-17T11:05:37.500624Z greg  $
 
 use strict;
 use warnings;
 
-<<<<<<< .working
-use Dic -command;
-sub usage_desc { "dic conversation -c CTEST -t TOPIC -s STORY -f FORM -p PAPER" }
-||||||| .merge-left.r2933
-sub usage_desc { "dic text -c CTEST -t TOPIC -s STORY -f FORM -p PAPER" }
-=======
-sub usage_desc { "dic text -c CTEST -t TOPIC -s STORY -f FORM -p PAPER -u 
+sub usage_desc { "dic conversation -c CTEST -t TOPIC -s STORY -f FORM -p PAPER -u
 	UNCLOZEABLE" }
->>>>>>> .merge-right.r2949
 
 sub opt_spec  {
 	return (
@@ -38,16 +25,8 @@ use IO::All;
 use YAML qw/LoadFile/;
 use Parse::RecDescent;
 use Text::Template;
-<<<<<<< .working
-use Dic::Cloze::Conversation qw/cloze/;
-use List::Util qw/shuffle/;
-||||||| .merge-left.r2933
-use Dic::Cloze::Text qw/cloze/;
+use Dic::Cloze::Conversation qw/simple_cloze cloze/;
 use List::Util qw/any shuffle/;
-=======
-use Dic::Cloze::Text qw/simple_cloze cloze/;
-use List::Util qw/any shuffle/;
->>>>>>> .merge-right.r2949
 
 our $RD_HINT = 1;
 
@@ -57,14 +36,7 @@ our $RD_HINT = 1;
 # my %ids = map { $_->{name} => $_->{id} } @members;
 # my %names = map { $_->{id} => $_->{name} } @members;
 
-<<<<<<< .working
-sub execute {
-	my ($self, $opt, $args) = @_;
-||||||| .merge-left.r2933
-my ($course, $cloze_style, $topic, $story, $form, $size) = @ARGV;
-=======
 my ($course, $cloze_style, $topic, $story, $form, $size, $unclozeable) = @ARGV;
->>>>>>> .merge-right.r2949
 
 	my ($course, $cloze_style, $topic, $story, $form, $size) = @$opt{qw/c z t s f p/};
 	my ($text_list, $question) = LoadFile
@@ -132,17 +104,6 @@ my ($course, $cloze_style, $topic, $story, $form, $size, $unclozeable) = @ARGV;
 		, 8 => "Eight", 9 => "Nine", 10 => "Ten", 11 =>"Eleven" 
 	);
 
-	$identifier = "$opt->{s}-$opt->{f}";
-	my @text = grep { $_->[0] eq $identifier } @$text_list;
-	die "No texts or more than 1 text called $identifier\n" if @text != 1;
-
-<<<<<<< .working
-	my $lines = $text[0][4];
-	my @lines = split /\n/, $lines;
-	my $unclozeables = $text[0][5];
-	my $text = cloze($cloze_style, $unclozeables, @lines);
-	my $textA = $text->{A};
-	my $textB = $text->{B};
 	for my $j ( 0) {
 		for my $i ( 0 .. $paper->{$size}->{i}) {
 			$tmplString .= "
@@ -157,32 +118,7 @@ my ($course, $cloze_style, $topic, $story, $form, $size, $unclozeable) = @ARGV;
 \\dicX$opt->{s}X$romanize{$opt->{f}}Xcard
 {$textB}
 \\end{textblock}\n";
-||||||| .merge-left.r2933
-$identifier = "$story-$form";
-my @text = grep { $_->[0] eq $identifier } @$text_list;
-die "No texts or more than 1 text called $identifier\n" if @text != 1;
-
-my $lines = $text[0][4];
-my @lines = split /\n/, $lines;
-my $unclozeables = $text[0][5];
-my $text = cloze($cloze_style, $unclozeables, @lines);
-my $textA = $text->{A};
-my $textB = $text->{B};
-my $word = $text->{word};
 print "words=@$word";
-my $words;
-if ( $text[0][6] and ref $text[0][6] eq 'HASH') {
-	my $check = $text[0][6];
-	my @pos = keys %$check;
-	my ( %hashed_check, $count_check, %binned );
-	for my $pos ( @pos ) {
-		next unless $check->{$pos};
-		$hashed_check{$pos}{$_}++, $count_check++
-			for split ' ', $check->{$pos};
-		for my $word ( @$word ) {
-			push @{$binned{$pos}}, $word if
-				$hashed_check{$pos}{$word};
-=======
 $identifier = "$story-$form";
 my @text = grep { $_->[0] eq $identifier } @$text_list;
 die "No texts or more than 1 text called $identifier\n" if @text != 1;
@@ -226,25 +162,9 @@ if ( $text[0][6] and ref $text[0][6] eq 'HASH') {
 			my $Word = lc $word;
 			push @{$binned{$pos}}, $Word if
 				$hashed_check{$pos}{$Word};
->>>>>>> .merge-right.r2949
 		}
-<<<<<<< .working
 		$tmplString .= "
 \\begin{Large}" . ( $j+1 ) . "\\end{Large}\\newpage\n\n";
-||||||| .merge-left.r2933
-		die "No pos checks for pos=$pos, word=@$word?" unless
-			ref $binned{$pos} eq 'ARRAY';
-		$words .= "$pos: " . join ' ', sort @{$binned{$pos}};
-		$words .= "\\\\";
-	}
-	warn "pos check_count=$count_check, but words=" . @$word if
-		$count_check != @$word;
-}
-elsif (ref $word eq 'ARRAY') {
-	$words = join ' ', sort @$word;
-	$words .= "\\\\";
-}
-=======
 		die 
 "No pos checks for pos=$pos of @pos, word=@$word, text=$text->{A}?" unless
 			ref $binned{$pos} eq 'ARRAY';
@@ -260,16 +180,8 @@ elsif (ref $word eq 'ARRAY') {
 	$words = join ' ', sort @$word;
 	$words .= "\\\\";
 }
->>>>>>> .merge-right.r2949
 warn "pos check_count=$count_check, but clozed words=" . @$word;
 
-<<<<<<< .working
-
-||||||| .merge-left.r2933
-print $words;
-
-=======
->>>>>>> .merge-right.r2949
 	}
 	$tmplString .= '
 \end{document}
@@ -287,18 +199,8 @@ print $words;
 	open TEX, ">/home/$ENV{USER}/curriculum/$course/$opt->{t}/dic_$opt->{s}_$opt->{f}.tex" or die "No open on " . $opt->{t} . ": " . $!;
 	print TEX $template->fill_in( HASH => $quiz );
 
-<<<<<<< .working
-}
-
-||||||| .merge-left.r2933
-system "xelatex --output-directory=/home/$ENV{USER}/curriculum/$course/$topic \
-	/home/$ENV{USER}/curriculum/$course/$topic/dic_${story}_$form.tex && \
-	lftp -c 'open greg\@web.nuu.edu.tw && cd public_html/$course && \
-	put ./$topic/dic_${story}_$form.pdf'";
-=======
 #system "xelatex --output-directory=/home/$ENV{USER}/curriculum/$course/$topic \
 #	/home/$ENV{USER}/curriculum/$course/$topic/dic_${story}_$form.tex && \
 #	lftp -c 'open greg\@web.nuu.edu.tw && cd public_html/$course && \
 #	put ./$topic/dic_${story}_$form.pdf'";
->>>>>>> .merge-right.r2949
 1;
