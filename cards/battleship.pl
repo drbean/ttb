44aab81e48a7f073b1b0b18a02535db06dc16a89
drbean <drbean@freeshell.org>
2022-10-23 21:48:25 +0800

    remove_bingo_code,add_tabular,xn,yn

--- cards/battleship.pl
+++ cards/battleship.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Last Edit: 2022 Oct 22, 11:09:04 PM
+# Last Edit: 2022 Oct 23,  9:32:20 PM
 # $Id: /dic/branches/ctest/dic.pl 1263 2007-06-23T12:37:20.810966Z greg  $
 
 use strict;
@@ -10,7 +10,6 @@ use Getopt::Long;
 use Pod::Usage;
 use List::Util qw/sample sum/;
 use List::MoreUtils qw/natatime/;
-use Lingua::Han::PinYin;
 
 my $man = 0;
 my $help = 0;
@@ -51,16 +50,16 @@ my $latexString = <<"START_LATEX";
 ]
 % \\usepackage[absolute,noshowtext,showboxes]{textpos}
 \\usepackage[absolute,showboxes]{textpos}
-\\textblockorigin{0.00cm}{0.00cm} %HPLaserJet5000LE
+\\textblockorigin{0.00cm}{0.00cm}
 % \\usepackage{soul}
 \\pagestyle{empty}
 \\setlength{\\unitlength}{1cm}
 
 START_LATEX
 
-my @form = split ',', $f;
-
-for my $f (@form) {
+# my @form = split ',', $f;
+# 
+# for my $f (@form) {
 	$latexString .= <<"CARD_COMMAND"
 \\newcommand{\\battleshipX${s}X$roman{$f}Xcard}[5]{%
         \\vspace{0.1cm}
@@ -73,7 +72,7 @@ for my $f (@form) {
 }
 
 CARD_COMMAND
-}
+# }
 
 my $layout = { a7 => { latex => [
                         { page => 1, x => "0", y => "0" , xy => "0,0" },
@@ -132,97 +131,42 @@ if ( ref $battleship eq 'HASH' and exists $battleship->{pair} ) {
                 push @x, $pair->[$_]->[1] for (0..$#$pair);
                 push @y, $pair->[$_]->[0] for (0..$#$pair);
         }
-        die "Unequal x, y numbers. Also check order"
+        die "Unequal x, y numbers in form $f match. Also check order"
                 unless ( @x == @y );
 }
 elsif ( ref $battleship eq 'HASH' ) {
        @y = keys %$battleship;
        my @x = values %$battleship;
-       die "Unequal x, y numbers. Also check order"
+       die "Unequal x, y numbers in form $f flash. Also check order"
                unless ( @x == @y );
 }
 else {
 	my $ref = ref $battleship;
 	die "battleship $ref ref not a HASH";
 }
+my $xn = $#x + 1;
+my $yn = $#y + 1;
+my $column_width = 1/$xn;
+my $row_height = 1/$yn;
 
 for my $card ( 0 .. $n-1 ) {
         $latexString .=
 "\\TPshowboxestrue
 \\begin{textblock}{8}($latex->[$paging]->{xy})
 \\textblocklabel{picture$latex->[$paging]->{xy}}
-\\battleshipX${s}X$roman{$f}Xcard{}{\\battleshipX${s}X$roman{$f}XPrompt}{}{";
-	if ( $reverse ) {
-		my @call_all = (@call, @lost_call);
-		if ( $five or $four ) {
-			for my $pos ( 0 .. $#call_all ) {
-				my $cell = ( $latex->[$paging]->{x}
-					+ $grid->[$pos]->{x} ) . "," .
-					( $latex->[$paging]->{y}
-					+ $grid->[$pos]->{y} );
-				my $width = $five ? 1.6 : 2;
-				my $height = $five ? 0.016 : 0.02;
-				$latexString .=
-				"\\begin{textblock}{$width}($cell)
-				\\vspace*{$height\\paperheight}
-				\\center $call_all[$pos]
-				\\vspace*{$height\\paperheight}
-				\\vfill
-				\\end{textblock}";
-				}
-		}
-		else {
-			$latexString .= "\\large\n" if $reverse;
-			$latexString .= "\\begin{multicols}{4}";
-			$latexString .= (s/_/\\_/g, "- $_\\\\") for sort @call_all;
-			$latexString .= "\\end{multicols}";
-		}
+\\battleshipX${s}X$roman{$f}Xcard{}{\\battleshipX${s}X$roman{$f}XPrompt}{}{
+\\begin{tabular}{l*{$column_width\\pagewidth}}";
+
+	$latexString .= join " & ", @x;
+	$latexString .= "\\\\";
+
+	for my $y ( @y ) {
+		$latexString .= join " & ", '' x $yn;
 	}
-	else {
-		$latexString .= (s/_/\\_/g, "$_ ") for @call;
-		$latexString .= (s/_/\\_/g, "XX${_}XX ") for @lost_call;
+		$latexString .= "}{} \n \\end{textblock}\n \\TPshowboxesfalse \n";
+		&paging;
 	}
-        $latexString .= "}{} \n \\end{textblock}\n \\TPshowboxesfalse \n";
-        &paging;
-}
 
-my $card_n = $reverse? 0 : $n-2;
-for my $card ( 0 .. $card_n ) {
-        my @candidate = sample( set => \@clinchers );
-        my @presented = sample( set => \@pruned, sample_size => @pruned/2);
-        my ( @ordered, $it );
-        if  ( $card % 2 == 0 ) {
-                @ordered = sort {$a cmp $b} (@presented, @candidate);
-
-        }
-        else {
-                @ordered = sort {$b cmp $a} (@presented, @candidate);
-        }
-        $it = natatime 3, @ordered;
-
-        $latexString .= 
-"\\TPshowboxestrue
-\\begin{textblock}{8}($latex->[$paging]->{xy})
-\\textblocklabel{picture$latex->[$paging]->{xy}}
-\\bingoX${s}X$roman{$f}Xcard{}{\\bingoX${s}X$roman{$f}XIdentifier}{\\parbox{9.0cm}{";
-        while ( my @word = $it->() ) {
-                tr/_/~/ for @word;
-		$latexString .= "\\begin{multicols}{4}";
-		if ( $word[2] ) {
-			$latexString .= "- $word[0] \\\\ - $word[1] \\\\ - $word[2] \\hspace{0pt}\\\\";
-		}
-		elsif ( $word[1] ) {
-			$latexString .= "- $word[0] \\\\ - $word[1] \\hspace{0pt}\\\\";
-		}
-		else {
-			$latexString .= "- $word[0] \\hspace{0pt}\\\\";
-
-		}
-		$latexString .= "\\end{multicols}";
-        }
-        $latexString .= "}}{}{} \n \\end{textblock}\n \\TPshowboxesfalse \n";
-        &paging;
-}
 $latexString .= "\\end{document}\n";
 
 my $bio = io "$ARGV[0]/bingo_${s}_$f.tex";
